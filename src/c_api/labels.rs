15421f84584346913a1ea1f36965b50d35ee8036
Luthaf <luthaf@luthaf.fr>
2022-05-19 15:56:53 +0200

    Allow to pass empty eqs_labels_t to rust functions

--- src/c_api/labels.rs
+++ src/c_api/labels.rs
@@ -33,8 +33,12 @@ impl std::convert::TryFrom<&eqs_labels_t> for Labels {
     type Error = Error;
 
     fn try_from(labels: &eqs_labels_t) -> Result<Labels, Self::Error> {
-        if labels.names.is_null() || labels.values.is_null() {
-            return Err(Error::InvalidParameter("labels.names and labels.values can not be NULL in eqs_labels_t".into()))
+        if labels.names.is_null() {
+            return Err(Error::InvalidParameter("labels.names can not be NULL in eqs_labels_t".into()))
+        }
+
+        if labels.values.is_null() && labels.count > 0 {
+            return Err(Error::InvalidParameter("labels.values is NULL but labels.count is >0 in eqs_labels_t".into()))
         }
 
         let mut names = Vec::new();
