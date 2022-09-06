3fc23ad9343b1385c18aa5be32878ee098a4d085
Davide Tisi <47503434+DavideTisi@users.noreply.github.com>
2022-09-06 23:19:30 +0200

    Update _dispatch.py
    Corrected an error in the doc of dot, and in the pytorch.dot function.
    added two functions (they might be useful for solve):
     - linalg_inv -> compute the inverse matrix as in numpy.linalg.inv(array)
     - solve the linear system as in  torch.linalg.inv(array)

--- equistore/operations/_dispatch.py
+++ equistore/operations/_dispatch.py
@@ -32,7 +32,7 @@ def norm(array, axis=None):
 
 def dot(array1, array2):
     """Compute dot product of two arrays.
-    This function has the equivalent bheaviour of  ``np.dot(array1,array2)``.
+    This function has the equivalent bheaviour of  ``np.dot(array1,array2.T)``.
     For numpy array, it check if the arrays are 2D, in which case it uses the matmul,
     which is preferred according to the doc.
     For torch it uses ``torch.dot``
@@ -49,8 +49,37 @@ def dot(array1, array2):
         shape1 = array1.size()
         shape2 = array2.size()
         if len(shape1) == 1 and len(shape2) == 1:
-            return torch.dot(array1, array2)
+            return torch.dot(array1, array2.T)
         else:
             return torch.matmul(array1, torch.Traspose(array2))
+    else:
+        raise TypeError(UNKNOWN_ARRAY_TYPE)
+
+
+def linalg_inv(array, detach=False):
+    """Compute the (multiplicative) inverse of a matrix.
+    This function has the equivalent bheaviour of  ``numpy.linalg.inv(array)``.
+    """
+    if isinstance(array, np.ndarray):
+        return np.linalg.inv(array)
+    elif isinstance(array, torch.Tensor):
+        result = torch.linalg.inv(array)
+        if detach:
+            result = result.detach()
+        return result
+    else:
+        raise TypeError(UNKNOWN_ARRAY_TYPE)
+
+
+def solve(array1, array2):
+    """Computes the solution of a square system of linear equations with a unique solution.
+    This function has the equivalent bheaviour of  ``numpy.linalg.solve(array1,array2)``.
+    """
+    # TODO not sure if it is usefull
+    if isinstance(array1, np.ndarray) and isinstance(array2, np.ndarray):
+        return np.linalg.solve(array1, array2)
+    elif isinstance(array1, TorchTensor) and isinstance(array2, TorchTensor):
+        result = torch.linalg.solve(array1, array2)
+        return result
     else:
         raise TypeError(UNKNOWN_ARRAY_TYPE)
