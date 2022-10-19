9e9087e42b52350bea5872325b869645c641d656
Davide Tisi <47503434+DavideTisi@users.noreply.github.com>
2022-10-19 23:51:52 +0200

    deleted normalize

--- equistore/operations/__init__.py
+++ equistore/operations/__init__.py
@@ -1,5 +1,4 @@
 from .dot import dot  # noqa
 from .lstsq import lstsq  # noqa
-from .normalize import normalize_by_sample  # noqa
 from .remove_gradients import remove_gradients  # noqa
 from .solve import solve  # noqa
