e36442027762c0ad427650a29f85c6a0838d57f1
Davide Tisi <davide.tisi93@gmail.com>
2022-11-09 17:38:20 +0100

    Add operations to work with TensorMap (#51)
    This adds a couple of function to solve linear systems and then make prediction, with all the data in 
    TensorMap form.

---
+++ python/src/equistore/operations/dot.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+from ..tensor import TensorMap
+from ..block import TensorBlock
+
+from . import _dispatch
+from ._utils import _check_same_keys
+
+
+def dot(A: TensorMap, B: TensorMap) -> TensorMap:
+    """
+    Computes the dot product of two :py:class:`TensorMap`.
+
+    The two :py:class:`TensorMap` must have the same ``keys``. The resulting
+    :py:class:`TensorMap` will have the same keys as the input and each block
+    will be the dot product of the two corresponding :py:class:`TensorBlock` in
+    the input.
+
+    :py:class:`TensorBlocks` corresponding to the same key must have the same
+    ``properties``. The resulting :py:class:`TensorBlocks` of the dot product of
+    two :py:class:`TensorBlocks` has ``result_block.values = block1.values @
+    block2.values.T``
+
+    :param A: first :py:class:`TensorMap` to multiply
+    :param B: second :py:class:`TensorMap` to multiply
+
+    :return: a :py:class:`TensorMap` with the same keys of ``A`` and ``B``, and
+            where each :py:class:`TensorBlock` has: the ``sample`` equal to the
+            ``sample`` of ``A``; the ``properties`` equal to the ``sample`` of
+            ``B``; and the ``components`` equal to the ``components`` of ``A``
+    """
+    _check_same_keys(A, B, "dot")
+
+    blocks = []
+    for key, block1 in A:
+        block2 = B.block(key)
+        blocks.append(_dot_block(block1=block1, block2=block2))
+
+    return TensorMap(A.keys, blocks)
+
+
+def _dot_block(block1: TensorBlock, block2: TensorBlock) -> TensorBlock:
+    if not np.all(block1.properties == block2.properties):
+        raise ValueError("TensorBlocks in `dot` should have the same properties")
+
+    if len(block2.components) > 0:
+        raise ValueError("the second TensorMap in `dot` should not have components")
+
+    if len(block2.gradients_list()) > 0:
+        raise ValueError("the second TensorMap in `dot` should not have gradients")
+
+    # values = block1.values @ block2.values.T
+    values = _dispatch.dot(block1.values, block2.values)
+
+    result_block = TensorBlock(
+        values=values,
+        samples=block1.samples,
+        components=block1.components,
+        properties=block2.samples,
+    )
+
+    for parameter, gradient in block1.gradients():
+        # gradient_data = gradient.data @ block2.values.T
+        gradient_data = _dispatch.dot(gradient.data, block2.values)
+
+        result_block.add_gradient(
+            parameter,
+            gradient_data,
+            gradient.samples,
+            gradient.components,
+        )
+
+    return result_block
---
+++ python/tests/operations/remove_gradients.py
@@ -0,0 +1,37 @@
+import os
+import unittest
+
+import equistore.operations as fn
+import equistore.io
+
+DATA_ROOT = os.path.join(os.path.dirname(__file__), "..", "data")
+
+
+class TestRemoveGradients(unittest.TestCase):
+    def test_remove_everything(self):
+        tensor = equistore.io.load(
+            os.path.join(DATA_ROOT, "qm7-power-spectrum.npz"),
+            # the npz is using DEFLATE compression, equistore only supports STORED
+            use_numpy=True,
+        )
+
+        self.assertEqual(tensor.block(0).gradients_list(), ["cell", "positions"])
+
+        tensor = fn.remove_gradients(tensor)
+        self.assertEqual(tensor.block(0).gradients_list(), [])
+
+    def test_remove_subset(self):
+        tensor = equistore.io.load(
+            os.path.join(DATA_ROOT, "qm7-power-spectrum.npz"),
+            # the npz is using DEFLATE compression, equistore only supports STORED
+            use_numpy=True,
+        )
+
+        self.assertEqual(tensor.block(0).gradients_list(), ["cell", "positions"])
+
+        tensor = fn.remove_gradients(tensor, ["positions"])
+        self.assertEqual(tensor.block(0).gradients_list(), ["cell"])
+
+
+if __name__ == "__main__":
+    unittest.main()
