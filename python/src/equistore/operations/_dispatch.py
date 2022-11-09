b2d8b02894ce4dda9882c2c7c6ee09782762e185
Luthaf <luthaf@luthaf.fr>
2022-11-09 15:56:25 +0100

    Minor cleanup in _dispatch

--- python/src/equistore/operations/_dispatch.py
+++ python/src/equistore/operations/_dispatch.py
@@ -28,78 +28,75 @@ def norm(array, axis=None):
         raise TypeError(UNKNOWN_ARRAY_TYPE)
 
 
-def dot(array1, array2):
+def dot(A, B):
     """Compute dot product of two arrays.
-    This function has the equivalent behaviour of  ``np.dot(array1,array2.T)``.
-    For numpy array, it check if the arrays are 2D, in which case it uses the matmul,
-    which is preferred according to the doc.
-    For torch it uses ``torch.dot``
-    if the arrays are 1D, ``torch.matmul`` is used otherwise.
+
+    This function has the same behavior as  ``np.dot(A, B.T)``, and assumes the
+    second array is 2-dimensional.
     """
-    if isinstance(array1, np.ndarray) and isinstance(array2, np.ndarray):
-        shape1 = array1.shape
-        shape2 = array2.shape
-        if len(shape1) == 2 and len(shape2) == 2:
-            return array1 @ array2.T
+    if isinstance(A, np.ndarray) and isinstance(B, np.ndarray):
+        shape1 = A.shape
+        assert len(B.shape) == 2
+        # Using matmul/@ is the recommended way in numpy docs for 2-dimensional
+        # matrices
+        if len(shape1) == 2:
+            return A @ B.T
         else:
-            return np.dot(array1, np.transpose(array2))
-    elif isinstance(array1, TorchTensor) and isinstance(array2, TorchTensor):
-        shape1 = array1.size()
-        shape2 = array2.size()
-        if len(shape1) == 1 and len(shape2) == 1:
-            return torch.dot(array1, array2.T)
-        else:
-            return array1 @ array2.T
+            return np.dot(A, B.T)
+    elif isinstance(A, TorchTensor) and isinstance(B, TorchTensor):
+        assert len(B.shape) == 2
+        return A @ B.T
     else:
         raise TypeError(UNKNOWN_ARRAY_TYPE)
 
 
-def solve(array1, array2):
-    """Computes the solution of a square system of linear equations
-    with a unique solution.
-    This function has the equivalent
-    behaviour of  ``numpy.linalg.solve(array1,array2)``.
+def solve(X, Y):
     """
-    if isinstance(array1, np.ndarray) and isinstance(array2, np.ndarray):
-        return np.linalg.solve(array1, array2)
-    elif isinstance(array1, TorchTensor) and isinstance(array2, TorchTensor):
-        result = torch.linalg.solve(array1, array2)
+    Computes the solution of a square system of linear equations with a unique
+    solution.
+
+    This function has the same behavior as ``numpy.linalg.solve(X, Y)``.
+    """
+    if isinstance(X, np.ndarray) and isinstance(Y, np.ndarray):
+        return np.linalg.solve(X, Y)
+    elif isinstance(X, TorchTensor) and isinstance(Y, TorchTensor):
+        result = torch.linalg.solve(X, Y)
         return result
     else:
         raise TypeError(UNKNOWN_ARRAY_TYPE)
 
 
-def lstsq(array1, array2, rcond, driver=None):
+def lstsq(X, Y, rcond, driver=None):
     """
-    Computes a solution to the least squares problem
-    of a system of linear equations.
-    Computes the vector x that approximately
-    solves the equation ``array1 @ x = array2``.
-    This function has the equivalent
-    behaviour of ``numpy.linalg.lstsq(array1,array2)``.
+    Computes a solution to the least squares problem of a system of linear
+    equations.
 
-    rcond: Cut-off ratio for small singular values of a.
-    WARNING: the default rcond=None for numpy and torch is different
-    numpy -> rcond is the machine precision times max(M, N).
-             with M, N being the dimensions of array1
-    torch -> rcond is the machine precision,
-             to have this behaviour in numpy use
-             rcond=-1
+    Computes the vector x that approximately solves the equation ``array1 @ x =
+    array2``. This function has the same behavior as ``numpy.linalg.lstsq(X,
+    Y)``.
 
-    driver: Used only in torch (ignored if numpy id used).
+    :param rcond: Cut-off ratio for small singular values of a.
+        WARNING: the default rcond=None for numpy and torch is different
+        numpy -> rcond is the machine precision times max(M, N).
+                with M, N being the dimensions of array1
+        torch -> rcond is the machine precision,
+                to have this behaviour in numpy use
+                rcond=-1
+
+    :param driver: Used only in torch (ignored if numpy id used).
             It chooses the LAPACK/MAGMA function that will be used.
-            Possible values: for CPU ‘gels’, ‘gelsy’, ‘gelsd, ‘gelss’.
-                             for GPU  the only valid driver is ‘gels’,
+            Possible values: for CPU 'gels', 'gelsy', 'gelsd', 'gelss'.
+                             for GPU  the only valid driver is 'gels',
                              which assumes that A is full-rank
             see https://pytorch.org/docs/stable/generated/torch.linalg.lstsq.html
             for a full description
-            If None, ‘gelsy’ is used for CPU inputs
-            and ‘gels’ for CUDA inputs. Default: None
+            If None, 'gelsy' is used for CPU inputs
+            and 'gels' for CUDA inputs. Default: None
     """
-    if isinstance(array1, np.ndarray) and isinstance(array2, np.ndarray):
-        return np.linalg.lstsq(array1, array2, rcond=rcond)[0]
-    elif isinstance(array1, TorchTensor) and isinstance(array2, TorchTensor):
-        result = torch.linalg.lstsq(array1, array2, rcond=rcond, driver=driver)[0]
+    if isinstance(X, np.ndarray) and isinstance(Y, np.ndarray):
+        return np.linalg.lstsq(X, Y, rcond=rcond)[0]
+    elif isinstance(X, TorchTensor) and isinstance(Y, TorchTensor):
+        result = torch.linalg.lstsq(X, Y, rcond=rcond, driver=driver)[0]
         return result
     else:
         raise TypeError(UNKNOWN_ARRAY_TYPE)
@@ -107,14 +104,8 @@ def lstsq(array1, array2, rcond, driver=None):
 
 def vstack(arrays):
     """Stack vertically a group of arrays.
-    This function has the equivalent
-    behaviour of ``numpy.vstack(arrays)``.
 
-    Args:
-        arrays : sequence of arrays
-
-    Returns:
-        array : vertical-stacked array
+    This function has the same behavior as ``numpy.vstack(arrays)``.
     """
     if isinstance(arrays[0], np.ndarray):
         return np.vstack(arrays)
