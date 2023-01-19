f11fb45f410d3c07309c7403c4a9cd4b73ca366c
Philip Loche <ploche@physik.fu-berlin.de>
2023-01-19 13:50:14 +0100

    Add LICENSE and AUTHORS

--- setup.py
+++ setup.py
@@ -160,6 +160,7 @@ def get_version():
 
 setup(
     version=get_version(),
+    author=", ".join(open(os.path.join(ROOT, "AUTHORS")).read().splitlines()),
     ext_modules=[
         # only declare the extension, it is built & copied as required by cmake
         # in the build_ext command
