66b5dff9b1edc0410f0b22c20c01c4615f19042f
Joseph Abbott <joseph.william.abbott@gmail.com>
2023-02-10 11:30:24 +0100

    rm logic fxns

--- python/src/equistore/metaoperations/__init__.py
+++ python/src/equistore/metaoperations/__init__.py
@@ -2,14 +2,10 @@
 The Python API for equistore also provides functions which operate on
 tensor metadata stored in :py:class:`equistore.Labels` objects.
 """
-from .equal import equal  # noqa
-from .intersection import intersection  # noqa
 from .unique import unique, unique_block  # noqa
 
 
 __all__ = [
-    "equal",
-    "intersection",
     "unique",
     "unique_block",
 ]
