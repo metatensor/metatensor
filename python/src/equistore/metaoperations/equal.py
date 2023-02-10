72471f311ddb7caf67ee3d80fc1601a4ba9c2aaa
Joseph Abbott <joseph.william.abbott@gmail.com>
2023-02-10 11:30:14 +0100

    rm logic stubs

--- python/src/equistore/metaoperations/equal.py
+++
@@ -1,9 +0,0 @@
-from ..labels import Labels
-
-
-def equal(a: Labels, b: Labels) -> bool:
-    """
-    Compares 2 Labels objects, returns true if exactly equivalent and false
-    otherwise.
-    """
-    return
--- python/src/equistore/metaoperations/intersection.py
+++
@@ -1,9 +0,0 @@
-from ..labels import Labels
-
-
-def intersection(a: Labels, b: Labels) -> Labels:
-    """
-    Compares 2 Labels objects, returns a Labels object of indices in the set
-    intersection of them.
-    """
-    return
