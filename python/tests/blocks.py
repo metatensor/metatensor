2366dcb2bfca5fb47735d83aeda69d8f5380aeed
Philip Loche <ploche@physik.fu-berlin.de>
2023-03-05 22:34:23 +0100

    Port Python core tests to pytest

--- python/tests/blocks.py
+++ python/tests/blocks.py
@@ -1,11 +1,10 @@
-import unittest
-
 import numpy as np
+from numpy.testing import assert_equal
 
 from equistore import Labels, TensorBlock
 
 
-class TestBlocks(unittest.TestCase):
+class TestBlocks:
     def test_repr(self):
         block = TensorBlock(
             values=np.full((3, 2), -1.0),
@@ -18,7 +17,7 @@ class TestBlocks(unittest.TestCase):
     components (): []
     properties (2): ['properties']
     gradients: no"""
-        self.assertTrue(block.__repr__() == expected)
+        assert block.__repr__() == expected
 
     def test_block_no_components(self):
         block = TensorBlock(
@@ -28,20 +27,20 @@ class TestBlocks(unittest.TestCase):
             properties=Labels(["properties"], np.array([[5], [3]], dtype=np.int32)),
         )
 
-        self.assertTrue(np.all(block.values == np.full((3, 2), -1.0)))
+        assert_equal(block.values, np.full((3, 2), -1.0))
 
-        self.assertEqual(block.samples.names, ("samples",))
-        self.assertEqual(len(block.samples), 3)
-        self.assertEqual(tuple(block.samples[0]), (0,))
-        self.assertEqual(tuple(block.samples[1]), (2,))
-        self.assertEqual(tuple(block.samples[2]), (4,))
+        assert block.samples.names == ("samples",)
+        assert len(block.samples) == 3
+        assert tuple(block.samples[0]) == (0,)
+        assert tuple(block.samples[1]) == (2,)
+        assert tuple(block.samples[2]) == (4,)
 
-        self.assertEqual(len(block.components), 0)
+        assert len(block.components) == 0
 
-        self.assertEqual(block.properties.names, ("properties",))
-        self.assertEqual(len(block.properties), 2)
-        self.assertEqual(tuple(block.properties[0]), (5,))
-        self.assertEqual(tuple(block.properties[1]), (3,))
+        assert block.properties.names == ("properties",)
+        assert len(block.properties) == 2
+        assert tuple(block.properties[0]) == (5,)
+        assert tuple(block.properties[1]) == (3,)
 
     def test_block_with_components(self):
         block = TensorBlock(
@@ -58,34 +57,34 @@ class TestBlocks(unittest.TestCase):
     components (3, 2): ['component_1', 'component_2']
     properties (2): ['properties']
     gradients: no"""
-        self.assertTrue(block.__repr__() == expected)
+        assert block.__repr__() == expected
 
-        self.assertTrue(np.all(block.values == np.full((3, 3, 2, 2), -1.0)))
+        assert_equal(block.values, np.full((3, 3, 2, 2), -1.0))
 
-        self.assertEqual(block.samples.names, ("samples",))
-        self.assertEqual(len(block.samples), 3)
-        self.assertEqual(tuple(block.samples[0]), (0,))
-        self.assertEqual(tuple(block.samples[1]), (2,))
-        self.assertEqual(tuple(block.samples[2]), (4,))
+        assert block.samples.names == ("samples",)
+        assert len(block.samples) == 3
+        assert tuple(block.samples[0]) == (0,)
+        assert tuple(block.samples[1]) == (2,)
+        assert tuple(block.samples[2]) == (4,)
 
-        self.assertEqual(len(block.components), 2)
+        assert len(block.components) == 2
         component_1 = block.components[0]
-        self.assertEqual(component_1.names, ("component_1",))
-        self.assertEqual(len(component_1), 3)
-        self.assertEqual(tuple(component_1[0]), (-1,))
-        self.assertEqual(tuple(component_1[1]), (0,))
-        self.assertEqual(tuple(component_1[2]), (1,))
+        assert component_1.names == ("component_1",)
+        assert len(component_1) == 3
+        assert tuple(component_1[0]) == (-1,)
+        assert tuple(component_1[1]) == (0,)
+        assert tuple(component_1[2]) == (1,)
 
         component_2 = block.components[1]
-        self.assertEqual(component_2.names, ("component_2",))
-        self.assertEqual(len(component_2), 2)
-        self.assertEqual(tuple(component_2[0]), (-4,))
-        self.assertEqual(tuple(component_2[1]), (1,))
+        assert component_2.names == ("component_2",)
+        assert len(component_2) == 2
+        assert tuple(component_2[0]) == (-4,)
+        assert tuple(component_2[1]) == (1,)
 
-        self.assertEqual(block.properties.names, ("properties",))
-        self.assertEqual(len(block.properties), 2)
-        self.assertEqual(tuple(block.properties[0]), (5,))
-        self.assertEqual(tuple(block.properties[1]), (3,))
+        assert block.properties.names, ("properties",)
+        assert len(block.properties) == 2
+        assert tuple(block.properties[0]) == (5,)
+        assert tuple(block.properties[1]) == (3,)
 
     def test_gradients(self):
         block = TensorBlock(
@@ -115,12 +114,12 @@ class TestBlocks(unittest.TestCase):
     components (3, 2): ['component_1', 'component_2']
     properties (2): ['properties']
     gradients: ['parameter']"""
-        self.assertTrue(block.__repr__() == expected)
+        assert block.__repr__() == expected
 
-        self.assertTrue(block.has_gradient("parameter"))
-        self.assertFalse(block.has_gradient("something else"))
+        assert block.has_gradient("parameter")
+        assert not block.has_gradient("something else")
 
-        self.assertEqual(block.gradients_list(), ["parameter"])
+        assert block.gradients_list() == ["parameter"]
 
         gradient = block.gradient("parameter")
 
@@ -129,14 +128,14 @@ parameter: 'parameter'
 samples (2): ['sample', 'parameter']
 components (3, 2): ['component_1', 'component_2']
 properties (2): ['properties']"""
-        self.assertTrue(gradient.__repr__() == expected_grad)
+        assert gradient.__repr__() == expected_grad
 
-        self.assertEqual(gradient.samples.names, ("sample", "parameter"))
-        self.assertEqual(len(gradient.samples), 2)
-        self.assertEqual(tuple(gradient.samples[0]), (0, -2))
-        self.assertEqual(tuple(gradient.samples[1]), (2, 3))
+        assert gradient.samples.names == ("sample", "parameter")
+        assert len(gradient.samples) == 2
+        assert tuple(gradient.samples[0]) == (0, -2)
+        assert tuple(gradient.samples[1]) == (2, 3)
 
-        self.assertTrue(np.all(gradient.data == np.full((2, 3, 2, 2), 11.0)))
+        assert_equal(gradient.data, np.full((2, 3, 2, 2), 11.0))
 
     def test_copy(self):
         block = TensorBlock(
@@ -152,15 +151,11 @@ properties (2): ['properties']"""
 
         del block
 
-        self.assertNotEqual(id(copy.values), block_values_id)
+        assert id(copy.values) != block_values_id
 
-        self.assertTrue(np.all(copy.values == np.full((3, 3, 2), 2.0)))
-        self.assertEqual(copy.samples.names, ("samples",))
-        self.assertEqual(len(copy.samples), 3)
-        self.assertEqual(tuple(copy.samples[0]), (0,))
-        self.assertEqual(tuple(copy.samples[1]), (2,))
-        self.assertEqual(tuple(copy.samples[2]), (4,))
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert_equal(copy.values, np.full((3, 3, 2), 2.0))
+        assert copy.samples.names == ("samples",)
+        assert len(copy.samples) == 3
+        assert tuple(copy.samples[0]) == (0,)
+        assert tuple(copy.samples[1]) == (2,)
+        assert tuple(copy.samples[2]) == (4,)
