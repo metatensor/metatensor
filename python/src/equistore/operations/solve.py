29c9cfd0f638b41cfa4f063779d923f3e516edf8
frostedoyster <bigi.f@libero.it>
2023-03-15 07:48:07 +0100

    Moved example

--- python/src/equistore/operations/solve.py
+++ python/src/equistore/operations/solve.py
@@ -22,6 +22,62 @@ def solve(X: TensorMap, Y: TensorMap) -> TensorMap:
             and where each :py:class:`TensorBlock` has: the ``sample``
             equal to the ``properties`` of ``Y``;
             and the ``properties`` equal to the ``properties`` of ``X``.
+
+    Here is an example using this function:
+
+    >>> import numpy as np
+    >>> import equistore
+    >>> from equistore import TensorBlock, TensorMap, Labels
+    ...
+    >>> X_values = np.random.rand(100, 2)
+    >>> y_values = (X_values @ np.array([10.0, 42.0]) + 0.1*np.random.normal(size=(100,))).reshape((100, 1))
+    ...
+    >>> covariance = X_values.T @ X_values 
+    >>> y_regression = X_values.T @ y_values
+    ...
+    >>> X = TensorMap(
+    ...     keys = Labels(
+    ...         names = ["dummy"],
+    ...         values = np.array([[0]])
+    ...     ),
+    ...     blocks = [TensorBlock(
+    ...         samples = Labels(
+    ...             names = ["sample"],
+    ...             values = np.arange(0, 2).reshape(2, 1)
+    ...         ),
+    ...         components = [],
+    ...         properties = Labels(
+    ...             names = ["property"],
+    ...             values = np.arange(0, 2).reshape(2, 1)
+    ...         ),
+    ...         values = covariance
+    ...     )]
+    ... )
+    ...
+    >>> y = TensorMap(
+    ...     keys = Labels(
+    ...         names = ["dummy"],
+    ...         values = np.array([[0]])
+    ...     ),
+    ...     blocks = [TensorBlock(
+    ...         samples = Labels(
+    ...             names = ["sample"],
+    ...             values = np.arange(0, 2).reshape(2, 1)
+    ...         ),
+    ...         components = [],
+    ...         properties = Labels(
+    ...             names = ["property"],
+    ...             values = np.arange(0, 1).reshape(1, 1)
+    ...         ),
+    ...         values = y_regression
+    ...     )]
+    ... )
+    ...
+    >>> c = equistore.solve(X, y)
+    ...
+    >>> print(c.block().values)
+    [[ 9.98104199 41.99906203]]
+
     """
     _check_maps(X, Y, "solve")
 
