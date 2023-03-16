b38f20d5e1592bdd08cfa9b2710f4b7181fcaa87
victorprincipe <victorprincipe1206@gmail.com>
2023-03-16 10:05:46 +0100

    Fixed doctests/examples in allclose.py

--- python/src/equistore/operations/allclose.py
+++ python/src/equistore/operations/allclose.py
@@ -31,8 +31,9 @@ def allclose(
 
     Here are examples using this function:
 
+    >>> from equistore import Labels
     >>> # Create simple block
-    ... block1 = TensorBlock(
+    >>> block1 = TensorBlock(
     ...    values=np.array([
     ...         [1, 2, 4],
     ...         [3, 5, 6],
@@ -49,10 +50,8 @@ def allclose(
     ...        ["properties"], np.array([[0], [1], [2]])
     ...     ),
     ... )
-    ...
-    ...
-    ... # Create a second block that is equivalent to block1
-    ... block2 = TensorBlock(
+    >>> # Create a second block that is equivalent to block1
+    >>> block2 = TensorBlock(
     ...     values=np.array([
     ...          [1, 2, 4],
     ...          [3, 5, 6],
@@ -70,20 +69,20 @@ def allclose(
     ...      ),
     ... )
     ...
-    ... # Create tensors from blocks, using keys with different names
-    ... keys1 = Labels(names=["key1"], values=np.array([[0]]))
-    ... keys2 = Labels(names=["key2"], values=np.array([[0]]))
+    >>> # Create tensors from blocks, using keys with different names
+    >>> keys1 = Labels(names=["key1"], values=np.array([[0]]))
+    >>> keys2 = Labels(names=["key2"], values=np.array([[0]]))
     ...
-    ... tensor1 = TensorMap(keys1, [block1])
-    ... tensor2 = TensorMap(keys2, [block2])
+    >>> tensor1 = TensorMap(keys1, [block1])
+    >>> tensor2 = TensorMap(keys2, [block2])
     ...
     >>> # Call allclose, which should fail as the blocks have different keys
-    ... # associated with them
+    >>> # associated with them
     >>> allclose(tensor1, tensor2)
     False
     >>> # Create a third tensor, which differs from tensor1 only by 1e-5 in a
-    ... # single block value
-    ... block3 = TensorBlock(
+    >>> # single block value
+    >>> block3 = TensorBlock(
     ...     values=np.array([
     ...          [1+1e-5, 2, 4],
     ...          [3, 5, 6],
@@ -97,22 +96,22 @@ def allclose(
     ...      ),
     ...     components=[],
     ...     properties=Labels(
-    ...         ["property_1"], np.array([[0], [1], [2]])
+    ...         ["properties"], np.array([[0], [1], [2]])
     ...      ),
     ... )
     ...
-    ... #Create tensors from blocks, using key with same name as block1
-    ... keys3 = Labels(names=["key1"], values=np.array([[0]]))
+    >>> #Create tensors from blocks, using key with same name as block1
+    >>> keys3 = Labels(names=["key1"], values=np.array([[0]]))
     ...
-    ... tensor3 = TensorMap(keys3, [block3])
+    >>> tensor3 = TensorMap(keys3, [block3])
     >>> # Call allclose, which should return False because the default rtol
-    ... # is 1e-13, and the difference in the first value between the blocks
-    ... # of the two tensors is 1e-5
+    >>> # is 1e-13, and the difference in the first value between the blocks
+    >>> # of the two tensors is 1e-5
     >>> allclose(tensor1, tensor3)
     False
     >>> # Calling allclose again with the optional argument rtol=1e-5 should
-    ... # return True, as the difference in the first value between the blocks
-    ... # of the two tensors is within the tolerance limit
+    >>> # return True, as the difference in the first value between the blocks
+    >>> # of the two tensors is within the tolerance limit
     >>> allclose(tensor1, tensor3, rtol=1e-5)
     True
 
@@ -152,8 +151,9 @@ def allclose_raise(
 
     Here are examples using this function:
 
+    >>> from equistore import Labels
     >>> # Create simple block, with one np.nan value
-    ... block1 = TensorBlock(
+    >>> block1 = TensorBlock(
     ...     values=np.array([
     ...          [1, 2, 4],
     ...          [3, 5, np.nan],
@@ -171,9 +171,9 @@ def allclose_raise(
     ...      ),
     ... )
     ...
-    ... # Create a second block that differs from block1 by 1e-5 in its
-    ... # first value
-    ... block2 = TensorBlock(
+    >>> # Create a second block that differs from block1 by 1e-5 in its
+    >>> # first value
+    >>> block2 = TensorBlock(
     ...     values=np.array([
     ...          [1+1e-5, 2, 4],
     ...          [3, 5, np.nan],
@@ -191,24 +191,28 @@ def allclose_raise(
     ...      ),
     ... )
     ...
-    ... #Create tensors from blocks, using same keys
-    ... keys = Labels(names=["key"], values=np.array([[0]]))
+    >>> #Create tensors from blocks, using same keys
+    >>> keys = Labels(names=["key"], values=np.array([[0]]))
     ...
-    ... tensor1 = TensorMap(keys, [block1])
-    ... tensor2 = TensorMap(keys, [block2])
+    >>> tensor1 = TensorMap(keys, [block1])
+    >>> tensor2 = TensorMap(keys, [block2])
     ...
-    >>> # Call allclose_raise, which should return two ValueErrors because:
-    ... # 1. The two NaNs are not considered equal of the two tensors is 1e-5,
-    ... # 2. The difference between the first value in the blocks
-    ... #    which is greater than the default rtol of 1e-13
+    >>> # Call allclose_raise, which should return a ValueError because:
+    >>> # 1. The two NaNs are not considered equal of the two tensors is 1e-5,
+    >>> # 2. The difference between the first value in the blocks
+    >>> #    which is greater than the default rtol of 1e-13
+    >>> # If this is executed yourself, you will see a nested exception
+    >>> # ValueError which explains that the values of the two blocks
+    >>> # are not allclose
     >>> allclose_raise(tensor1, tensor2)
-    ValueError: values are not allclose
-    ...
+    Traceback (most recent call last):
+        ...
+    ValueError: The TensorBlocks with key = (0,) are different
     >>> # Call allclose_raise again, but use equal_nan=True and rtol=1e-5
-    ... # This passes, as the two NaNs are now considered equal, and the
-    ... # difference between the first values of the blocks of the two tensors
-    ... # is within the rtol limit of 1e-5
-    >>>  allclose_raise(tensor1, tensor2, equal_nan=True, rtol=1e-5)
+    >>> # This passes, as the two NaNs are now considered equal, and the
+    >>> # difference between the first values of the blocks of the two tensors
+    >>> # is within the rtol limit of 1e-5
+    >>> allclose_raise(tensor1, tensor2, equal_nan=True, rtol=1e-5)
 
     :param tensor1: first :py:class:`TensorMap`.
     :param tensor2: second :py:class:`TensorMap`.
@@ -254,8 +258,10 @@ def allclose_block(
     ``True`` if no exception is rased, `False` otherwise.
 
     Here are some exmaples using this function:
+
+    >>> from equistore import Labels
     >>> # Create simple block
-    ... block1 = TensorBlock(
+    >>> block1 = TensorBlock(
     ...     values=np.array([
     ...          [1, 2, 4],
     ...          [3, 5, 6],
@@ -273,8 +279,8 @@ def allclose_block(
     ...      ),
     ... )
     ...
-    ... # Recreate block1, but change first value in the block from 1 to 1.00001
-    ... block2 = TensorBlock(
+    >>> # Recreate block1, but change first value in the block from 1 to 1.00001
+    >>> block2 = TensorBlock(
     ...     values=np.array([
     ...          [1+1e-5, 2, 4],
     ...          [3, 5, 6],
@@ -293,13 +299,13 @@ def allclose_block(
     ... )
     ...
     >>> # Call allclose_block, which should return False because the default
-    ... # rtol is 1e-13, and the difference in the first value between the
-    ... # two blocks is 1e-5
+    >>> # rtol is 1e-13, and the difference in the first value between the
+    >>> # two blocks is 1e-5
     >>> allclose_block(block1, block2)
     False
     >>> # Calling allclose_block with the optional argument rtol=1e-5 should
-    ... # return True, as the difference in the first value between the two
-    ... # blocks is within the tolerance limit
+    >>> # return True, as the difference in the first value between the two
+    >>> # blocks is within the tolerance limit
     >>> allclose_block(block1, block2, rtol=1e-5)
     True
 
@@ -340,8 +346,9 @@ def allclose_block_raise(
 
     Here is an example using this function:
 
+    >>> from equistore import Labels
     >>> #Create simple block
-    ... block1 = TensorBlock(
+    >>> block1 = TensorBlock(
     ...     values=np.array([
     ...          [1, 2, 4],
     ...          [3, 5, 6],
@@ -358,10 +365,9 @@ def allclose_block_raise(
     ...         ["property_1"], np.array([[0], [1], [2]])
     ...      ),
     ... )
-    ... keys = Labels(names=["key"], values=np.array([[0]]))
     ...
-    ... #Recreate block1, but rename properties label 'property_1' to 'property_2'
-    ... block2 = TensorBlock(
+    >>> #Recreate block1, but rename properties label 'property_1' to 'property_2'
+    >>> block2 = TensorBlock(
     ...     values=np.array([
     ...          [1, 2, 4],
     ...          [3, 5, 6],
@@ -380,8 +386,10 @@ def allclose_block_raise(
     ... )
     ...
     >>> # Call allclose_block_raise, which should raise a ValueError because the
-    ... # properties of the two blocks are not equal
+    >>> # properties of the two blocks are not equal
     >>> allclose_block_raise(block1, block2)
+    Traceback (most recent call last):
+        ...
     ValueError: Inputs to 'allclose' should have the same properties:
     properties names are not the same or not in the same order.
 
