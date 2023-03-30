5a3ccdefcd8e6cc7dd40b6971dbf46255804d9f5
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2023-03-30 16:31:09 +0200

    Added example to the equistore.operations.lstsq docstring (#193)
    Reviewed-by: Luthaf <luthaf@luthaf.fr>
    Reviewed-by: Alexander Goscinski <alex.goscinski@posteo.de>

--- python/src/equistore/operations/lstsq.py
+++ python/src/equistore/operations/lstsq.py
@@ -9,25 +9,104 @@ from .equal_metadata import _check_maps, _check_same_gradients
 
 
 def lstsq(X: TensorMap, Y: TensorMap, rcond, driver=None) -> TensorMap:
-    """
+    r"""
     Solve a linear system using two :py:class:`TensorMap`.
 
-    Return the least-squares solution to a linear equation ``Y = X * w`` solving
-    for ``w``, where ``Y``, ``X`` and ``w`` are all :py:class:`TensorMap`. ``Y``
-    and ``X`` must have the same ``keys``.
+    The least-squares solution ``w_b`` for the linear system :math:`X_b w_b =
+    Y_b` is solved for all blocks :math:`b` in ``X`` and ``Y``. ``X`` and ``Y``
+    must have the same keys. The returned :py:class:`TensorMap` ``w`` has the
+    same keys as ``X`` and ``Y``, and stores in each block the least-squares
+    solutions :math:`w_b`.
+
+    If a block has multiple components, they are moved to the "samples" axis
+    before solving the linear system.
+
+    If gradients are present, they must be present in both ``X`` and ``Y``.
+    Gradients are concatenated with the block values along the "samples" axis,
+    :math:`A_b = [X_b, {\nabla} X_b]`, :math:`B_b = [Y_b, {\nabla} Y_b]`, and
+    the linear system :math:`A_b w_b = B_b` is solved for :math:`w_b` using
+    least-squares.
+
+    .. note::
+      The solutions :math:`w_b` differ from the output of numpy or torch in that they
+      are already transposed. Be aware of that if you want to manually access
+      the values of blocks of ``w`` (see also the example below).
+
+    Here is an example using this function:
+
+    >>> import numpy as np
+    >>> from equistore import Labels, TensorBlock, TensorMap
+    >>> import equistore
+    ...
+    >>> values_X = np.array([
+    ...     [1.0, 2.0],
+    ...     [3.0, 1.0],
+    ... ])
+    ...
+    >>> values_Y = np.array([
+    ...     [1.0, 0.0],
+    ...     [0.0, 1.0],
+    ... ])
+    ...
+    >>> samples = Labels(
+    ...     ["structure"],
+    ...     np.array([[0], [1]]),
+    ... )
+    ...
+    >>> components = []
+    ...
+    >>> properties = Labels(
+    ...     ["properties"],
+    ...     np.array([[0], [1]])
+    ... )
+    ...
+    >>> keys = Labels(names=["key"], values=np.array([[0]]))
+    ...
+    >>> block_X = TensorBlock(
+    ...     values_X, samples, components, properties,
+    ... )
+    ...
+    >>> block_Y = TensorBlock(
+    ...     values_Y, samples, components, properties,
+    ... )
+    ...
+    >>> X = TensorMap(keys, [block_X])
+    ...
+    >>> Y = TensorMap(keys, [block_Y])
+    ...
+    >>> w = equistore.lstsq(X, Y, rcond=1e-10)
+    ...
+    >>> # Note: we take the transpose here
+    >>> y = X.block(0).values @ w.block(0).values.T
+    ...
+    >>> # Set small entries in y to 0, they are numerical noise
+    >>> mask = np.abs(y) < 1e-15
+    >>> y[mask] = 0.0
+    >>> print(y)
+    [[1. 0.]
+     [0. 1.]]
+
 
     :param X: a :py:class:`TensorMap` containing the "coefficient" matrices.
-    :param Y: a :py:class:`TensorMap` containing the "dependent variable" values.
-    :param rcond: Cut-off ratio for small singular values of a. None chose the
-                default value for numpy or PyTorch
+    :param Y: a :py:class:`TensorMap` containing the "dependent variable"
+        values.
+    :param rcond: Cut-off ratio for small singular values of a. The singular
+        value :math:`{\sigma}_i` is treated as zero if smaller than
+        :math:`r_{cond}{\sigma}_1`, where :math:`{\sigma}_1` is the biggest
+        singular value of :math:`X_b`. ``None`` choses the default value for numpy
+        or PyTorch.
     :param driver: Used only in torch (ignored if numpy is used), see
-        https://pytorch.org/docs/stable/generated/torch.linalg.lstsq.html
-        for a full description
+        https://pytorch.org/docs/stable/generated/torch.linalg.lstsq.html for a
+        full description
 
     :return: a :py:class:`TensorMap` with the same keys of ``Y`` and ``X``, and
-            where each :py:class:`TensorBlock` has: the ``sample`` equal to the
-            ``properties`` of ``Y``; and the ``properties`` equal to the
-            ``properties`` of ``X``.
+        where each :py:class:`TensorBlock` has: the ``sample`` equal to the
+        ``properties`` of ``Y``; and the ``properties`` equal to the
+        ``properties`` of ``X``.
+
+    :raises ValueError: if the order in the samples or components does not match
+        between ``X`` and ``Y``.
+
     """
     if rcond is None:
         warnings.warn(
