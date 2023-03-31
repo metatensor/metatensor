5410c488b005e5d3512dcd1b04d381f38f6dd1f5
frostedoyster <bigi.f@libero.it>
2023-03-31 10:16:20 +0200

    Format typo

--- python/tests/operations/reduce_over_samples.py
+++ python/tests/operations/reduce_over_samples.py
@@ -529,7 +529,7 @@ class TestReductionAllSamples(unittest.TestCase):
                     [-1.3, 26.7, 4.54],
                 ]
             ),
-            samples = Labels.arange("samples", 3),
+            samples=Labels.arange("samples", 3),
             components=[],
             properties=Labels(["properties"], np.array([[0], [1], [5]])),
         )
@@ -1010,7 +1010,7 @@ class TestZeroSamples(unittest.TestCase):
 
         sum_block = TensorBlock(
             values=np.array([[-0.3, 28.7, 8.54], [6.5, 10.3, 12.87]]),
-            samples = Labels.arange("samples1", 2),
+            samples=Labels.arange("samples1", 2),
             components=[],
             properties=Labels(["properties"], np.array([[0], [1], [5]])),
         )
