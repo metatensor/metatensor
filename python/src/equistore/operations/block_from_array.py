f1580f468ccc36688698f21b42e91e5183732d0c
Luthaf <luthaf@luthaf.fr>
2023-04-04 13:42:51 +0200

    Small docstring improvements

--- python/src/equistore/operations/block_from_array.py
+++ python/src/equistore/operations/block_from_array.py
@@ -3,32 +3,32 @@ from equistore import Labels, TensorBlock
 
 
 def block_from_array(array: equistore.data.Array) -> TensorBlock:
-    """Returns a simple TensorBlock from an array.
+    """
+    Creates a simple TensorBlock from an array.
 
-    The metadata in the resulting ``TensorBlock`` is filled with
-    ranges of integers. This function should be seen as a quick
-    way of creating a ``TensorBlock`` from arbitrary data. However,
-    the metadata generated in this way has little meaning.
+    The metadata in the resulting :py:class:`TensorBlock` is filled with ranges
+    of integers. This function should be seen as a quick way of creating a
+    :py:class:`TensorBlock` from arbitrary data. However, the metadata generated
+    in this way has little meaning.
 
-    :param array: An array with two or more dimensions. This can
-        either be a `np.ndarray` or a `torch.Tensor`.
+    :param array: An array with two or more dimensions. This can either be a
+        :py:class:`numpy.ndarray` or a :py:class:`torch.Tensor`.
 
-    :return: A ``TensorBlock`` whose values correspond to the
-        provided ``array``. Its metadata consists of ``Labels`` whose
-        names are ``sample``, ``component1``, ``component2``, ...,
-        ``property``, in this order. The number of
-        ``component`` labels is adapted to the dimensionality of the
-        input array. The metadata associated with each label is a range
-        of integers going from 0 to the dimensionality of the
-        corresponding axis (not included). The output ``TensorBlock``
-        has no gradients.
+    :return: A :py:class:`TensorBlock` whose values correspond to the provided
+        ``array``. The metadata names are set to ``"sample"`` for samples;
+        ``"component_1"``, ``"component_2"``, ... for components; and
+        ``property`` for properties. The number of ``component`` labels is
+        adapted to the dimensionality of the input array. The metadata
+        associated with each label is a range of integers going from 0 to the
+        size of the corresponding axis. The returned :py:class:`TensorBlock` has
+        no gradients.
 
     Here is an example using this function:
 
     >>> import numpy as np
     >>> import equistore
     ...
-    >>> # Construct a simple 3D array:
+    >>> # Construct a simple 4D array:
     >>> array = np.linspace(0, 10, 42).reshape((7, 3, 1, 2))
     >>> # Transform it into a TensorBlock:
     >>> tensor_block = equistore.block_from_array(array)
@@ -39,7 +39,7 @@ def block_from_array(array: equistore.data.Array) -> TensorBlock:
         properties (2): ['property']
         gradients: no
     >>> # The data inside the TensorBlock will correspond to the provided array:
-    >>> print(np.allclose(array, tensor_block.values))
+    >>> print(np.all(array == tensor_block.values))
     True
     """
 
