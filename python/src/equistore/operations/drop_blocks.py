1b6afb15a9adb8555c8e76c4aabd3f8cff91e54a
Joseph W. Abbott <joseph.william.abbott@gmail.com>
2023-04-06 16:39:31 +0200

    Add a copy flag to `drop_blocks` (#248)

--- python/src/equistore/operations/drop_blocks.py
+++ python/src/equistore/operations/drop_blocks.py
@@ -1,24 +1,44 @@
 import numpy as np
 
-from equistore import Labels, TensorMap
+from equistore import Labels, TensorBlock, TensorMap
 
 
-def drop_blocks(tensor: TensorMap, keys: Labels) -> TensorMap:
+def drop_blocks(tensor: TensorMap, keys: Labels, copy: bool = False) -> TensorMap:
     """
     Drop specified key/block pairs from a TensorMap.
 
     :param tensor: the TensorMap to drop the key-block pair from.
-    :param keys: a :py:class:`Labels` object containing the keys of the
-        blocks to drop
+    :param keys: a :py:class:`Labels` object containing the keys of the blocks
+        to drop
+    :param copy: if ``True``, the returned :py:class:`TensorMap` is constructed
+        by copying the blocks from the input `tensor`. If ``False`` (default),
+        the values of the blocks in the output :py:class:`TensorMap` reference
+        the same data as the input `tensor`. The latter can be useful for
+        limiting memory usage, but should be used with caution when manipulating
+        the underlying data.
 
-    :return: the input :py:class:`TensorMap` with the specified key/block
-        pairs dropped.
+    :return: the input :py:class:`TensorMap` with the specified key/block pairs
+        dropped.
     """
+    # Check arg types
+    if not isinstance(tensor, TensorMap):
+        raise TypeError(
+            f"input `tensor` must be a TensorMap, got '{type(tensor)}' instead"
+        )
+    if not isinstance(keys, Labels):
+        raise TypeError(
+            f"input `keys` must be a Labels object, got '{type(keys)}' instead"
+        )
+    if not isinstance(copy, bool):
+        raise TypeError(f"`copy` flag must be a boolean, got '{type(copy)}' instead")
+
     if not np.all(tensor.keys.names == keys.names):
         raise ValueError(
             "The input tensor's keys must have the same names as the specified"
             f" keys to drop. Should be {tensor.keys.names} but got {keys.names}"
         )
+
+    # Find the difference between key of the original tensor and those to drop
     diff = np.setdiff1d(keys, tensor.keys)
     if len(diff) > 0:
         raise ValueError(
@@ -26,5 +46,28 @@ def drop_blocks(tensor: TensorMap, keys: Labels) -> TensorMap:
             f" Non-existent keys: {diff}"
         )
     new_keys = np.setdiff1d(tensor.keys, keys)
-    new_blocks = [tensor[key].copy() for key in new_keys]
+
+    # Create the new TensorMap
+    if copy:
+        new_blocks = [tensor[key].copy() for key in new_keys]
+    else:
+        new_blocks = []
+        for key in new_keys:
+            # Create new block
+            new_block = TensorBlock(
+                values=tensor[key].values,
+                samples=tensor[key].samples,
+                components=tensor[key].components,
+                properties=tensor[key].properties,
+            )
+            # Add gradients
+            for parameter, gradient in tensor[key].gradients():
+                new_block.add_gradient(
+                    parameter=parameter,
+                    data=gradient.data,
+                    samples=gradient.samples,
+                    components=gradient.components,
+                )
+            new_blocks.append(new_block)
+
     return TensorMap(keys=new_keys, blocks=new_blocks)
--- python/tests/operations/equal_metadata.py
+++ python/tests/operations/equal_metadata.py
@@ -10,6 +10,9 @@ from equistore import Labels, TensorBlock, TensorMap, equal_metadata
 DATA_ROOT = os.path.join(os.path.dirname(__file__), "..", "data")
 
 
+# ===== Fixtures =====
+
+
 @pytest.fixture
 def test_tensor_map_1() -> TensorMap:
     return equistore.io.load(
@@ -105,207 +108,219 @@ def tensor_map() -> TensorMap:
     return TensorMap(keys, [block_1, block_2, block_3, block_4])
 
 
-class TestEqualMetaData:
-    def test_self(self, test_tensor_map_1):
-        # check if the metadata of the same tensor are equal
-        assert equal_metadata(test_tensor_map_1, test_tensor_map_1)
+# ===== Tests =====
 
-    def test_two_tesnors(self, test_tensor_map_1, test_tensor_map_2):
-        # check if the metadata of two tensor maps are equal
-        assert not equal_metadata(test_tensor_map_1, test_tensor_map_2)
 
-    def test_after_drop(self, test_tensor_map_1):
-        # check if dropping an existing block changes the metadata
-        new_key = Labels(
-            test_tensor_map_1.keys.names, np.array([tuple(test_tensor_map_1.keys[0])])
+def test_self(test_tensor_map_1):
+    # check if the metadata of the same tensor are equal
+    assert equal_metadata(test_tensor_map_1, test_tensor_map_1)
+
+
+def test_two_tensors(test_tensor_map_1, test_tensor_map_2):
+    # check if the metadata of two tensor maps are equal
+    assert not equal_metadata(test_tensor_map_1, test_tensor_map_2)
+
+
+def test_after_drop(test_tensor_map_1):
+    # check if dropping an existing block changes the metadata
+    new_key = Labels(
+        test_tensor_map_1.keys.names, np.array([tuple(test_tensor_map_1.keys[0])])
+    )
+    new_tesnor = equistore.drop_blocks(test_tensor_map_1, new_key)
+    assert not equal_metadata(test_tensor_map_1, new_tesnor)
+
+
+def test_single_nonexisting_meta(test_tensor_map_1, test_tensor_map_2):
+    # check behavior if non existing metadata is provided
+    # wrong metadata key alone
+    wrong_meta = "species"
+    error_message = f"Invalid metadata to check: {wrong_meta}"
+    with pytest.raises(ValueError, match=error_message):
+        equal_metadata(
+            tensor_1=test_tensor_map_1,
+            tensor_2=test_tensor_map_1,
+            check=[wrong_meta],
         )
-        new_tesnor = equistore.drop_blocks(test_tensor_map_1, new_key)
-        assert not equal_metadata(test_tensor_map_1, new_tesnor)
+    with pytest.raises(ValueError, match=error_message):
+        equal_metadata(
+            tensor_1=test_tensor_map_1,
+            tensor_2=test_tensor_map_2,
+            check=[wrong_meta],
+        )
+    # wrong metadata key with another correct one
+    correct_meta = "properties"
+    with pytest.raises(ValueError, match=error_message):
+        equal_metadata(
+            tensor_1=test_tensor_map_1,
+            tensor_2=test_tensor_map_1,
+            check=[correct_meta, wrong_meta],
+        )
+    with pytest.raises(ValueError, match=error_message):
+        equal_metadata(
+            tensor_1=test_tensor_map_1,
+            tensor_2=test_tensor_map_2,
+            check=[correct_meta, wrong_meta],
+        )
 
-    def test_single_nonexisting_meta(self, test_tensor_map_1, test_tensor_map_2):
-        # check behavior if non existing metadata is provided
-        # wrong metadata key alone
-        wrong_meta = "species"
-        error_message = f"Invalid metadata to check: {wrong_meta}"
-        with pytest.raises(ValueError, match=error_message):
-            equal_metadata(
-                tensor_1=test_tensor_map_1,
-                tensor_2=test_tensor_map_1,
-                check=[wrong_meta],
-            )
-        with pytest.raises(ValueError, match=error_message):
-            equal_metadata(
-                tensor_1=test_tensor_map_1,
-                tensor_2=test_tensor_map_2,
-                check=[wrong_meta],
-            )
-        # wrong metadata key with another correct one
-        correct_meta = "properties"
-        with pytest.raises(ValueError, match=error_message):
-            equal_metadata(
-                tensor_1=test_tensor_map_1,
-                tensor_2=test_tensor_map_1,
-                check=[correct_meta, wrong_meta],
-            )
-        with pytest.raises(ValueError, match=error_message):
-            equal_metadata(
-                tensor_1=test_tensor_map_1,
-                tensor_2=test_tensor_map_2,
-                check=[correct_meta, wrong_meta],
-            )
 
-    def test_changing_tensor_key_order(self, test_tensor_map_1):
-        # check changing the key order
-        keys = test_tensor_map_1.keys
-        new_keys = keys[::-1]
-        new_blocks = [test_tensor_map_1[key].copy() for key in new_keys]
-        new_tensor = TensorMap(keys=new_keys, blocks=new_blocks)
-        assert equal_metadata(test_tensor_map_1, new_tensor)
+def test_changing_tensor_key_order(test_tensor_map_1):
+    # check changing the key order
+    keys = test_tensor_map_1.keys
+    new_keys = keys[::-1]
+    new_blocks = [test_tensor_map_1[key].copy() for key in new_keys]
+    new_tensor = TensorMap(keys=new_keys, blocks=new_blocks)
+    assert equal_metadata(test_tensor_map_1, new_tensor)
 
-    def test_changing_samples_key_order(self, test_tensor_map_1):
-        # changing the order of the values of the samples should yield False
-        new_blocks = []
-        for key in test_tensor_map_1.keys:
-            block = test_tensor_map_1[key].copy()
-            samples = block.samples[::-1]
+
+def test_changing_samples_key_order(test_tensor_map_1):
+    # changing the order of the values of the samples should yield False
+    new_blocks = []
+    for key in test_tensor_map_1.keys:
+        block = test_tensor_map_1[key].copy()
+        samples = block.samples[::-1]
+        new_block = TensorBlock(
+            values=block.values,
+            samples=samples,
+            properties=block.properties,
+            components=block.components,
+        )
+        for param, obj in block.gradients():
+            new_block.add_gradient(
+                parameter=param,
+                samples=obj.samples,
+                components=obj.components,
+                data=obj.data,
+            )
+        new_blocks.append(new_block)
+
+    new_tensor = TensorMap(keys=test_tensor_map_1.keys, blocks=new_blocks)
+    assert not equal_metadata(test_tensor_map_1, new_tensor)
+
+
+def test_changing_properties_key_order(test_tensor_map_1):
+    # changing the order of the values of the properties should yield False
+    new_blocks = []
+    for key in test_tensor_map_1.keys:
+        block = test_tensor_map_1[key].copy()
+        properties = block.properties[::-1]
+        new_block = TensorBlock(
+            values=block.values,
+            samples=block.samples,
+            properties=properties,
+            components=block.components,
+        )
+        for param, obj in block.gradients():
+            new_block.add_gradient(
+                parameter=param,
+                samples=obj.samples,
+                components=obj.components,
+                data=obj.data,
+            )
+        new_blocks.append(new_block)
+
+    new_tensor = TensorMap(keys=test_tensor_map_1.keys, blocks=new_blocks)
+    assert not equal_metadata(test_tensor_map_1, new_tensor)
+
+
+def test_add_components_key_order(tensor_map):
+    # changing the order of the values of the components should yield False
+    new_blocks = []
+    for key in tensor_map.keys:
+        block = tensor_map[key].copy()
+        components = [comp[::-1] for comp in block.components]
+        new_block = TensorBlock(
+            values=block.values,
+            samples=block.samples,
+            properties=block.properties,
+            components=components,
+        )
+        for param, obj in block.gradients():
+            new_block.add_gradient(
+                parameter=param,
+                samples=obj.samples,
+                components=components,
+                data=obj.data,
+            )
+        new_blocks.append(new_block)
+
+    new_tensor = TensorMap(keys=tensor_map.keys, blocks=new_blocks)
+    assert not equal_metadata(tensor_map, new_tensor)
+
+
+def test_remove_last_sample(tensor_map):
+    # removing the last sample should yield False
+    new_blocks = []
+    for key in tensor_map.keys:
+        block = tensor_map[key].copy()
+        new_block = TensorBlock(
+            values=block.values[:-1],
+            samples=block.samples[:-1],
+            properties=block.properties,
+            components=block.components,
+        )
+        for param, obj in block.gradients():
+            new_block.add_gradient(
+                parameter=param,
+                samples=obj.samples[:-1],
+                components=obj.components,
+                data=obj.data[:-1],
+            )
+        new_blocks.append(new_block)
+
+    new_tensor = TensorMap(keys=tensor_map.keys, blocks=new_blocks)
+    assert not equal_metadata(tensor_map, new_tensor)
+
+
+def test_remove_last_property(tensor_map):
+    # removing the last property should yield False
+    new_blocks = []
+    for key in tensor_map.keys:
+        block = tensor_map[key].copy()
+        new_block = TensorBlock(
+            values=block.values[..., :-1],
+            samples=block.samples,
+            properties=block.properties[..., :-1],
+            components=block.components,
+        )
+        for param, obj in block.gradients():
+            new_block.add_gradient(
+                parameter=param,
+                samples=obj.samples,
+                components=obj.components,
+                data=obj.data[..., :-1],
+            )
+        new_blocks.append(new_block)
+
+    new_tensor = TensorMap(keys=tensor_map.keys, blocks=new_blocks)
+    assert not equal_metadata(tensor_map, new_tensor)
+
+
+def test_remove_last_component(tensor_map):
+    # removing the last component should yield False
+    new_blocks = []
+    for key in tensor_map.keys:
+        block = tensor_map[key].copy()
+
+        can_remove = np.all([len(c) > 2 for c in block.components])
+
+        if can_remove:
+            components = [c[:-1] for c in block.components]
             new_block = TensorBlock(
-                values=block.values,
-                samples=samples,
-                properties=block.properties,
-                components=block.components,
-            )
-            for param, obj in block.gradients():
-                new_block.add_gradient(
-                    parameter=param,
-                    samples=obj.samples,
-                    components=obj.components,
-                    data=obj.data,
-                )
-            new_blocks.append(new_block)
-
-        new_tensor = TensorMap(keys=test_tensor_map_1.keys, blocks=new_blocks)
-        assert not equal_metadata(test_tensor_map_1, new_tensor)
-
-    def test_changing_properties_key_order(self, test_tensor_map_1):
-        # changing the order of the values of the properties should yield False
-        new_blocks = []
-        for key in test_tensor_map_1.keys:
-            block = test_tensor_map_1[key].copy()
-            properties = block.properties[::-1]
-            new_block = TensorBlock(
-                values=block.values,
-                samples=block.samples,
-                properties=properties,
-                components=block.components,
-            )
-            for param, obj in block.gradients():
-                new_block.add_gradient(
-                    parameter=param,
-                    samples=obj.samples,
-                    components=obj.components,
-                    data=obj.data,
-                )
-            new_blocks.append(new_block)
-
-        new_tensor = TensorMap(keys=test_tensor_map_1.keys, blocks=new_blocks)
-        assert not equal_metadata(test_tensor_map_1, new_tensor)
-
-    def test_add_components_key_order(self, tensor_map):
-        # changing the order of the values of the components should yield False
-        new_blocks = []
-        for key in tensor_map.keys:
-            block = tensor_map[key].copy()
-            components = [comp[::-1] for comp in block.components]
-            new_block = TensorBlock(
-                values=block.values,
+                values=block.values[:, :-1],
                 samples=block.samples,
                 properties=block.properties,
                 components=components,
             )
-            for param, obj in block.gradients():
+            for parameter, gradient in block.gradients():
                 new_block.add_gradient(
-                    parameter=param,
-                    samples=obj.samples,
-                    components=components,
-                    data=obj.data,
-                )
-            new_blocks.append(new_block)
-
-        new_tensor = TensorMap(keys=tensor_map.keys, blocks=new_blocks)
-        assert not equal_metadata(tensor_map, new_tensor)
-
-    def test_remove_last_sample(self, tensor_map):
-        # removing the last sample should yield False
-        new_blocks = []
-        for key in tensor_map.keys:
-            block = tensor_map[key].copy()
-            new_block = TensorBlock(
-                values=block.values[:-1],
-                samples=block.samples[:-1],
-                properties=block.properties,
-                components=block.components,
-            )
-            for param, obj in block.gradients():
-                new_block.add_gradient(
-                    parameter=param,
-                    samples=obj.samples[:-1],
-                    components=obj.components,
-                    data=obj.data[:-1],
-                )
-            new_blocks.append(new_block)
-
-        new_tensor = TensorMap(keys=tensor_map.keys, blocks=new_blocks)
-        assert not equal_metadata(tensor_map, new_tensor)
-
-    def test_remove_last_property(self, tensor_map):
-        # removing the last property should yield False
-        new_blocks = []
-        for key in tensor_map.keys:
-            block = tensor_map[key].copy()
-            new_block = TensorBlock(
-                values=block.values[..., :-1],
-                samples=block.samples,
-                properties=block.properties[..., :-1],
-                components=block.components,
-            )
-            for param, obj in block.gradients():
-                new_block.add_gradient(
-                    parameter=param,
-                    samples=obj.samples,
-                    components=obj.components,
-                    data=obj.data[..., :-1],
-                )
-            new_blocks.append(new_block)
-
-        new_tensor = TensorMap(keys=tensor_map.keys, blocks=new_blocks)
-        assert not equal_metadata(tensor_map, new_tensor)
-
-    def test_remove_last_component(self, tensor_map):
-        # removing the last component should yield False
-        new_blocks = []
-        for key in tensor_map.keys:
-            block = tensor_map[key].copy()
-
-            can_remove = np.all([len(c) > 2 for c in block.components])
-
-            if can_remove:
-                components = [c[:-1] for c in block.components]
-                new_block = TensorBlock(
-                    values=block.values[:, :-1],
-                    samples=block.samples,
-                    properties=block.properties,
+                    parameter=parameter,
+                    data=gradient.data[:, :-1],
+                    samples=gradient.samples,
                     components=components,
                 )
-                for parameter, gradient in block.gradients():
-                    new_block.add_gradient(
-                        parameter=parameter,
-                        data=gradient.data[:, :-1],
-                        samples=gradient.samples,
-                        components=components,
-                    )
-            else:
-                new_block = block.copy()
-            new_blocks.append(new_block)
+        else:
+            new_block = block.copy()
+        new_blocks.append(new_block)
 
-        new_tensor = TensorMap(keys=tensor_map.keys, blocks=new_blocks)
-        assert not equal_metadata(tensor_map, new_tensor)
+    new_tensor = TensorMap(keys=tensor_map.keys, blocks=new_blocks)
+    assert not equal_metadata(tensor_map, new_tensor)
