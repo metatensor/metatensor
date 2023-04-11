5ac387234fbd3ea0c8bd8cb66f2e542474b0847c
zhangyaolong <56628666+zhangylch@users.noreply.github.com>
2023-04-11 10:38:10 +0200

    Add example to the dot function (#200)
    Co-authored-by: Davide Tisi <47503434+DavideTisi@users.noreply.github.com>

--- python/src/equistore/operations/dot.py
+++ python/src/equistore/operations/dot.py
@@ -19,6 +19,45 @@ def dot(A: TensorMap, B: TensorMap) -> TensorMap:
     two :py:class:`TensorBlocks` has ``result_block.values = block1.values @
     block2.values.T``
 
+    >>> from equistore import Labels
+    >>> block1 = TensorBlock(
+    ...     values=np.array(
+    ...         [
+    ...             [1, 2, 3],
+    ...             [4, 5, 6],
+    ...         ]
+    ...     ),
+    ...     samples=Labels(["structure"], np.array([[0], [1]])),
+    ...     components=[],
+    ...     properties=Labels(["properties"], np.array([[0], [1], [2]])),
+    ... )
+    >>> block2 = TensorBlock(
+    ...     values=np.array(
+    ...         [
+    ...             [1, 2, 3],
+    ...             [4, 5, 6],
+    ...         ]
+    ...     ),
+    ...     samples=Labels(["structure"], np.array([[0], [1]])),
+    ...     components=[],
+    ...     properties=Labels(["properties"], np.array([[0], [1], [2]])),
+    ... )
+    >>> keys = Labels(names=["key"], values=np.array([[0]]))
+    >>> A = TensorMap(keys, [block1])
+    >>> B = TensorMap(keys, [block2])
+    >>> tensor_dot = dot(A, B)
+    >>> print(tensor_dot.block(0))
+    TensorBlock
+        samples (2): ['structure']
+        components (): []
+        properties (2): ['structure']
+        gradients: no
+    >>> print(tensor_dot.block(0).samples)
+    [(0,) (1,)]
+    >>> print(tensor_dot.block(0).values)
+    [[14 32]
+     [32 77]]
+
     :param A: first :py:class:`TensorMap` to multiply
     :param B: second :py:class:`TensorMap` to multiply
 
@@ -26,6 +65,7 @@ def dot(A: TensorMap, B: TensorMap) -> TensorMap:
             where each :py:class:`TensorBlock` has: the ``sample`` equal to the
             ``sample`` of ``A``; the ``properties`` equal to the ``sample`` of
             ``B``; and the ``components`` equal to the ``components`` of ``A``
+
     """
     _check_maps(A, B, "dot")
 
@@ -60,7 +100,6 @@ def _dot_block(block1: TensorBlock, block2: TensorBlock) -> TensorBlock:
     for parameter, gradient in block1.gradients():
         # gradient_data = gradient.data @ block2.values.T
         gradient_data = _dispatch.dot(gradient.data, block2.values)
-
         result_block.add_gradient(
             parameter,
             gradient_data,
