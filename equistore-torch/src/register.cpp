35a78548d1f728f2d3db3fbf77d061f1a31a0bd8
Luthaf <luthaf@luthaf.fr>
2023-05-10 19:40:16 +0200

    Update the Labels API, including all the convencience functions

--- equistore-torch/src/register.cpp
+++ equistore-torch/src/register.cpp
@@ -9,23 +9,90 @@
 using namespace equistore_torch;
 
 
-// There is no way to access the docstrings from Python, so we
-// don't bother setting them to something useful.
+// There is no way to access the docstrings from Python, so we don't bother
+// setting them to something useful.
 const std::string DOCSTRING = "";
 
 
+static TorchLabelsEntry labels_entry(const TorchLabels& self, int64_t index) {
+    return torch::make_intrusive<LabelsEntryHolder>(self, index);
+}
+
+// this function can not be implemented as a member of LabelsHolder, since it
+// needs to receive a `TorchLabels` to give it to the `LabelsEntryHolder`
+// constructor.
+static torch::IValue labels_getitem(const TorchLabels& self, torch::IValue index) {
+    if (index.isInt()) {
+        return labels_entry(self, index.toInt());
+    } else {
+        auto names = equistore_torch::details::normalize_names(std::move(index), "index");
+        return LabelsHolder::view(self, std::move(names));
+    }
+}
+
+
 TORCH_LIBRARY(equistore, m) {
+    m.class_<LabelsEntryHolder>("LabelsEntry")
+        .def("__str__", &LabelsEntryHolder::__repr__)
+        .def("__repr__", &LabelsEntryHolder::__repr__)
+        .def("__len__", &LabelsEntryHolder::size)
+        .def("__getitem__", &LabelsEntryHolder::getitem, DOCSTRING,
+            {torch::arg("index")}
+        )
+        .def("__eq__", [](const TorchLabelsEntry& self, const TorchLabelsEntry& other){ return *self == *other; },
+            DOCSTRING,
+            {torch::arg("other")}
+        )
+        .def("__ne__", [](const TorchLabelsEntry& self, const TorchLabelsEntry& other){ return *self != *other; },
+            DOCSTRING,
+            {torch::arg("other")}
+        )
+        .def_property("names", &LabelsEntryHolder::names)
+        .def_property("values", &LabelsEntryHolder::values)
+        ;
+
     m.class_<LabelsHolder>("Labels")
         .def(
             torch::init<torch::IValue, torch::Tensor>(), DOCSTRING,
             {torch::arg("names"), torch::arg("values")}
         )
+        .def("__str__", &LabelsHolder::__str__)
+        // __repr__ is ignored for now, until we can use
+        // https://github.com/pytorch/pytorch/pull/100724 (hopefully torch 2.1)
+        .def("__repr__", &LabelsHolder::__repr__)
         .def("__len__", &LabelsHolder::count)
+        .def("__contains__", [](const TorchLabels& self, torch::IValue entry){
+            return self->position(entry).has_value();
+            }, DOCSTRING,
+            {torch::arg("entry")}
+        )
+        .def("__eq__", [](const TorchLabels& self, const TorchLabels& other){ return *self == *other; },
+            DOCSTRING,
+            {torch::arg("other")}
+        )
+        .def("__ne__", [](const TorchLabels& self, const TorchLabels& other){ return *self != *other; },
+            DOCSTRING,
+            {torch::arg("other")}
+        )
+        .def("__getitem__", labels_getitem, DOCSTRING, {torch::arg("index")})
+        .def_static("single", &LabelsHolder::single)
+        .def_static("empty", &LabelsHolder::empty)
+        .def_static("range", &LabelsHolder::range)
+        .def("entry", labels_entry, DOCSTRING, {torch::arg("index")})
+        .def("view", [](const TorchLabels& self, torch::IValue names) {
+            auto names_vector = equistore_torch::details::normalize_names(std::move(names), "names");
+            return LabelsHolder::view(self, std::move(names_vector));
+        }, DOCSTRING, {torch::arg("names")})
         .def_property("names", &LabelsHolder::names)
         .def_property("values", &LabelsHolder::values)
         .def("position", &LabelsHolder::position, DOCSTRING,
             {torch::arg("entry")}
         )
+        .def("print", &LabelsHolder::print, DOCSTRING,
+            {torch::arg("max_entries"), torch::arg("indent") = 0}
+        )
+        .def("is_view", &LabelsHolder::is_view)
+        .def("to_owned", [](const TorchLabels& self){ return torch::make_intrusive<LabelsHolder>(self->to_owned()); })
         ;
 
     m.class_<TensorBlockHolder>("TensorBlock")
--- equistore-torch/tests/labels.cpp
+++ equistore-torch/tests/labels.cpp
@@ -42,4 +42,66 @@ TEST_CASE("Labels") {
         i = labels->position(std::vector<int64_t>{0, 4});
         CHECK_FALSE(i.has_value());
     }
+
+    SECTION("print") {
+        auto labels = LabelsHolder::create({"aaa", "bbb"}, {{1, 2}, {3, 4}});
+
+        auto expected = " aaa  bbb\n     1    2\n     3    4\n";
+        CHECK(labels->print(-1, 3) == expected);
+    }
+
+    SECTION("create views") {
+        auto labels = LabelsHolder::create({"aaa", "bbb"}, {{1, 2}, {3, 4}});
+
+        CHECK_FALSE(labels->is_view());
+
+        auto view = LabelsHolder::view(labels, {"aaa"});
+        CHECK(view->is_view());
+
+        CHECK(view->names().size() == 1);
+        CHECK(view->names()[0] == "aaa");
+
+        CHECK(view->values().index({0, 0}).item<int32_t>() == 1);
+        CHECK(view->values().index({1, 0}).item<int32_t>() == 3);
+
+        CHECK_THROWS_WITH(view->position(1), Catch::Matchers::Contains("can not call this function on Labels view, call to_owned first"));
+        auto owned = view->to_owned();
+        CHECK_FALSE(owned.is_view());
+        CHECK(owned.position(std::vector<int64_t>{1}).value() == 0);
+    }
+
+    SECTION("equality") {
+        auto labels_1 = LabelsHolder::create({"a", "b"}, {{0, 0}, {1, 0}});
+        auto labels_2 = LabelsHolder::create({"a", "b"}, {{0, 0}, {1, 0}});
+        auto labels_3 = LabelsHolder::create({"a", "c"}, {{0, 0}, {1, 0}});
+
+        // torch::intrusive_ptr operator== checks for object identity
+        CHECK(labels_1 != labels_2);
+
+        CHECK(*labels_1 == *labels_2);
+        CHECK(*labels_1 != *labels_3);
+    }
+}
+
+TEST_CASE("LabelsEntry") {
+
+    auto labels = LabelsHolder::create({"aaa", "bbb"}, {{1, 2}, {3, 4}});
+
+    auto entry = LabelsEntryHolder(labels, 0);
+    CHECK(entry.size() == 2);
+
+    CHECK(entry.names().size() == 2);
+    CHECK(entry.names()[0] == "aaa");
+    CHECK(entry.names()[1] == "bbb");
+
+    CHECK(entry.values()[0].item<int32_t>() == 1);
+    CHECK(entry.values()[1].item<int32_t>() == 2);
+
+    CHECK(entry.__repr__() == "LabelsEntry(aaa=1, bbb=2)");
+
+    CHECK(entry[0] == 1);
+    CHECK(entry[1] == 2);
+
+    CHECK(entry["aaa"] == 1);
+    CHECK(entry["bbb"] == 2);
 }
