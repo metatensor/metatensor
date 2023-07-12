8eb034f69e1c968fdb562cadda41301a5c7d5190
Luthaf <luthaf@luthaf.fr>
2023-07-12 12:08:54 +0200

    Better handling of exceptions in C++ and TorchScript
    `equistore::details::catch_exceptions` should help to prevent unwinding
    through the C API

--- equistore-torch/src/misc.cpp
+++ equistore-torch/src/misc.cpp
@@ -14,18 +14,24 @@ eqs_status_t equistore_torch::details::create_torch_array(
     uintptr_t shape_count,
     eqs_array_t* array
 ) {
-    auto sizes = std::vector<int64_t>();
-    for (size_t i=0; i<shape_count; i++) {
-        sizes.push_back(static_cast<int64_t>(shape_ptr[i]));
-    }
+    return equistore::details::catch_exceptions([](
+        const uintptr_t* shape_ptr,
+        uintptr_t shape_count,
+        eqs_array_t* array
+    ) {
+        auto sizes = std::vector<int64_t>();
+        for (size_t i=0; i<shape_count; i++) {
+            sizes.push_back(static_cast<int64_t>(shape_ptr[i]));
+        }
 
-    auto options = torch::TensorOptions().device(torch::kCPU).dtype(torch::kF64);
-    auto tensor = torch::zeros(sizes, options);
+        auto options = torch::TensorOptions().device(torch::kCPU).dtype(torch::kF64);
+        auto tensor = torch::zeros(sizes, options);
 
-    auto cxx_array = std::unique_ptr<equistore::DataArrayBase>(new TorchDataArray(tensor));
-    *array = equistore::DataArrayBase::to_eqs_array_t(std::move(cxx_array));
+        auto cxx_array = std::unique_ptr<equistore::DataArrayBase>(new TorchDataArray(tensor));
+        *array = equistore::DataArrayBase::to_eqs_array_t(std::move(cxx_array));
 
-    return EQS_SUCCESS;
+        return EQS_SUCCESS;
+    }, shape_ptr, shape_count, array);
 }
 
 
