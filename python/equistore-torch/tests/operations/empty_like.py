c59261f7ada5a0dac060de7a20e80b1ce60a15a3
frostedoyster <bigi.f@libero.it>
2023-08-21 09:13:49 +0200

    Appropriate naming of variables in tests

--- python/equistore-torch/tests/operations/empty_like.py
+++ python/equistore-torch/tests/operations/empty_like.py
@@ -7,11 +7,11 @@ from .data import load_data
 
 def test_operation_as_python():
     tensor = load_data("qm7-power-spectrum.npz")
-    zero_tensor = equistore.torch.empty_like(tensor)
-    assert equistore.torch.equal_metadata(zero_tensor, tensor)
+    empty_tensor = equistore.torch.empty_like(tensor)
+    assert equistore.torch.equal_metadata(empty_tensor, tensor)
 
 
 def test_operation_as_torch_script():
     tensor = load_data("qm7-power-spectrum.npz")
-    zero_tensor = torch.jit.script(equistore.torch.empty_like)(tensor)
-    assert equistore.torch.equal_metadata(zero_tensor, tensor)
+    empty_tensor = torch.jit.script(equistore.torch.empty_like)(tensor)
+    assert equistore.torch.equal_metadata(empty_tensor, tensor)
