c132581271129a21e5c9a7b6c80534e5bd58c519
frostedoyster <bigi.f@libero.it>
2023-08-22 16:50:27 +0200

    Fix `pow()` tests

--- python/equistore-torch/tests/operations/pow.py
+++ python/equistore-torch/tests/operations/pow.py
@@ -9,11 +9,15 @@ def test_operation_as_python():
     tensor = load_data("qm7-power-spectrum.npz")
     power_tensor = equistore.torch.pow(tensor, 2.0)
     assert equistore.torch.equal_metadata(power_tensor, tensor)
-    assert equistore.allclose(power_tensor, equistore.torch.multiply(tensor, tensor))
+    assert equistore.torch.allclose(
+        power_tensor, equistore.torch.multiply(tensor, tensor)
+    )
 
 
 def test_operation_as_torch_script():
     tensor = load_data("qm7-power-spectrum.npz")
     power_tensor = torch.jit.script(equistore.torch.pow)(tensor, 2.0)
     assert equistore.torch.equal_metadata(power_tensor, tensor)
-    assert equistore.allclose(power_tensor, equistore.torch.multiply(tensor, tensor))
+    assert equistore.torch.allclose(
+        power_tensor, equistore.torch.multiply(tensor, tensor)
+    )
