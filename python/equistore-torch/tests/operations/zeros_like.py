dd87c5bde341314e178f314a88bd3e00673559f9
Luthaf <luthaf@luthaf.fr>
2023-08-23 12:46:51 +0200

    Add a few more tests

--- python/equistore-torch/tests/operations/zeros_like.py
+++ python/equistore-torch/tests/operations/zeros_like.py
@@ -1,17 +1,32 @@
 import torch
+from packaging import version
 
 import equistore.torch
 
 from .data import load_data
 
 
-def test_operation_as_python():
+def check_operation(zeros_like):
     tensor = load_data("qm7-power-spectrum.npz")
-    zero_tensor = equistore.torch.zeros_like(tensor)
+    zero_tensor = zeros_like(tensor)
+
+    # right output type
+    assert isinstance(zero_tensor, torch.ScriptObject)
+    if version.parse(torch.__version__) >= version.parse("2.1"):
+        assert zero_tensor._type().name() == "TensorMap"
+
+    # right metadata
     assert equistore.torch.equal_metadata(zero_tensor, tensor)
+
+    # right values
+    for block in zero_tensor.blocks():
+        assert torch.all(block.values == 0)
+
+
+def test_operation_as_python():
+    check_operation(equistore.torch.zeros_like)
 
 
 def test_operation_as_torch_script():
-    tensor = load_data("qm7-power-spectrum.npz")
-    zero_tensor = torch.jit.script(equistore.torch.zeros_like)(tensor)
-    assert equistore.torch.equal_metadata(zero_tensor, tensor)
+    scripted = torch.jit.script(equistore.torch.zeros_like)
+    check_operation(scripted)
