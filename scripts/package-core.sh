7486b440bb4195513cc6c6e2228ef9e09c35cfd0
Luthaf <luthaf@luthaf.fr>
2023-09-25 15:36:53 +0200

    Run shellcheck on sh scripts

--- scripts/package-core.sh
+++ scripts/package-core.sh
@@ -4,13 +4,13 @@
 # Rust crate, and copy it to be included in the metatensor crate source release,
 # and the metatensor-core python package sdist.
 
-ROOT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
+ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)
 set -eux
 
-rm -rf $ROOT_DIR/target/package
-cd $ROOT_DIR/metatensor-core
+rm -rf "$ROOT_DIR/target/package"
+cd "$ROOT_DIR/metatensor-core"
 
 # Package metatensor-core and copy the .crate file
 cargo package --allow-dirty --no-verify
-cp $ROOT_DIR/target/package/metatensor-core-*.crate $ROOT_DIR/metatensor/
-cp $ROOT_DIR/target/package/metatensor-core-*.crate $ROOT_DIR/python/metatensor-core/
+cp "$ROOT_DIR"/target/package/metatensor-core-*.crate "$ROOT_DIR/metatensor/"
+cp "$ROOT_DIR"/target/package/metatensor-core-*.crate "$ROOT_DIR/python/metatensor-core/"
