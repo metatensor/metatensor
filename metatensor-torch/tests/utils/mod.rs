4b3a41de673e8c2daf4731e5a0cf13e7166dc882
Luthaf <luthaf@luthaf.fr>
2024-04-23 17:05:55 +0200

    Enable tests and build wheels with PyTorch v2.3

--- metatensor-torch/tests/utils/mod.rs
+++ metatensor-torch/tests/utils/mod.rs
@@ -84,7 +84,7 @@ pub fn setup_pytorch(build_dir: PathBuf) -> PathBuf {
         .expect("failed to run python");
     assert!(status.success(), "failed to run `python -m pip install --upgrade pip`");
 
-    let torch_version = std::env::var("METATENSOR_TESTS_TORCH_VERSION").unwrap_or("2.2.*".into());
+    let torch_version = std::env::var("METATENSOR_TESTS_TORCH_VERSION").unwrap_or("2.3.*".into());
     let status = Command::new(&python)
         .arg("-m")
         .arg("pip")
