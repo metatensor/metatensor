57678cda0aff2b3bf44a24698471d4114635266e
Sofiia Chorna <chernaya.sofiia@gmail.com>
2024-08-26 08:03:50 +0200

    Add "features" custom output
    This is intended for all cases where we want to use a featurization
    of a system: last layer features for prediction rigidity, collective
    variables in metadynamics, computing features for visualization, etc.

--- python/metatensor-torch/tests/atomistic/outputs.py
+++ python/metatensor-torch/tests/atomistic/outputs.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, Optional
 
+import pytest
 import torch
 
 from metatensor.torch import Labels, TensorBlock, TensorMap
@@ -13,8 +14,36 @@ from metatensor.torch.atomistic import (
 )
 
 
-class EnergyEnsembleModel(torch.nn.Module):
-    """A metatensor atomistic model returning an energy ensemble"""
+@pytest.fixture
+def system():
+    return System(
+        types=torch.tensor([1, 2, 3]),
+        positions=torch.tensor([[1, 1, 1], [2, 2, 2], [3, 3, 3]], dtype=torch.float64),
+        cell=torch.zeros([3, 3], dtype=torch.float64),
+    )
+
+
+@pytest.fixture
+def get_capabilities() -> callable:
+    def _create_capabilities(output_name: str) -> ModelCapabilities:
+        return ModelCapabilities(
+            length_unit="angstrom",
+            atomic_types=[1, 2, 3],
+            interaction_range=4.3,
+            outputs={output_name: ModelOutput(per_atom=False)},
+            supported_devices=["cpu"],
+            dtype="float64",
+        )
+
+    return _create_capabilities
+
+
+class BaseAtomisticModel(torch.nn.Module):
+    """Base class for metatensor atomistic models"""
+
+    def __init__(self, output_name: str):
+        super().__init__()
+        self.output_name = output_name
 
     def forward(
         self,
@@ -22,42 +51,37 @@ class EnergyEnsembleModel(torch.nn.Module):
         outputs: Dict[str, ModelOutput],
         selected_atoms: Optional[Labels] = None,
     ) -> Dict[str, TensorMap]:
-        assert "energy_ensemble" in outputs
-        assert not outputs["energy_ensemble"].per_atom
+        assert self.output_name in outputs
+        assert not outputs[self.output_name].per_atom
         assert selected_atoms is None
 
-        return_dict: Dict[str, TensorMap] = {}
         block = TensorBlock(
             values=torch.tensor([[0.0, 1.0, 2.0]] * len(systems), dtype=torch.float64),
             samples=Labels("system", torch.arange(len(systems)).reshape(-1, 1)),
             components=[],
             properties=Labels("energy", torch.tensor([[0], [1], [2]])),
         )
-        return_dict["energy_ensemble"] = TensorMap(
-            Labels("_", torch.tensor([[0]])), [block]
-        )
-        return return_dict
+        return {self.output_name: TensorMap(Labels("_", torch.tensor([[0]])), [block])}
 
 
-def test_energy_ensemble_model():
+class EnergyEnsembleModel(BaseAtomisticModel):
+    """A metatensor atomistic model returning an energy ensemble"""
+
+    def __init__(self):
+        super().__init__("energy_ensemble")
+
+
+class FeaturesModel(BaseAtomisticModel):
+    """A metatensor atomistic model returning features"""
+
+    def __init__(self):
+        super().__init__("features")
+
+
+def test_energy_ensemble_model(system, get_capabilities):
     model = EnergyEnsembleModel()
-
-    capabilities = ModelCapabilities(
-        length_unit="angstrom",
-        atomic_types=[1, 2, 3],
-        interaction_range=4.3,
-        outputs={"energy_ensemble": ModelOutput(per_atom=False)},
-        supported_devices=["cpu"],
-        dtype="float64",
-    )
-
+    capabilities = get_capabilities("energy_ensemble")
     atomistic = MetatensorAtomisticModel(model.eval(), ModelMetadata(), capabilities)
-
-    system = System(
-        types=torch.tensor([1, 2, 3]),
-        positions=torch.tensor([[1, 1, 1], [2, 2, 2], [3, 3, 3]], dtype=torch.float64),
-        cell=torch.zeros([3, 3], dtype=torch.float64),
-    )
 
     options = ModelEvaluationOptions(
         outputs={"energy_ensemble": ModelOutput(per_atom=False)}
@@ -72,3 +96,22 @@ def test_energy_ensemble_model():
     assert list(ensemble.block().values.shape) == [2, 3]
     assert ensemble.block().samples.names == ["system"]
     assert ensemble.block().properties.names == ["energy"]
+
+
+def test_features_model(system, get_capabilities):
+    model = FeaturesModel()
+    capabilities = get_capabilities("features")
+    atomistic = MetatensorAtomisticModel(model.eval(), ModelMetadata(), capabilities)
+
+    options = ModelEvaluationOptions(outputs={"features": ModelOutput(per_atom=False)})
+
+    result = atomistic([system, system], options, check_consistency=True)
+    assert "features" in result
+
+    features = result["features"]
+    assert features.keys == Labels("_", torch.tensor([[0]]))
+    assert list(features.block().values.shape) == [2, 3]
+    assert features.block().samples.names == ["system"]
+    assert features.block().properties.names == ["energy"]
+    assert features.block().components == []
+    assert len(result["features"].blocks()) == 1
