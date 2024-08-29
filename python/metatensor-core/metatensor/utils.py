f191cf1973b77b3b3c8d1d929373c8b614c55c93
Guillaume Fraux <guillaume.fraux@epfl.ch>
2024-08-29 15:33:00 +0200

    Allow to use an external version of libmetatensor from Python
    This will be used in the conda distribution of the Python package

--- python/metatensor-core/metatensor/utils.py
+++ python/metatensor-core/metatensor/utils.py
@@ -111,7 +111,17 @@ def _to_arguments_parse(context, *args, **kwargs):
     return dtype, device
 
 
-cmake_prefix_path = os.path.join(os.path.dirname(__file__), "lib", "cmake")
-"""
-Path containing the CMake configuration files for the underlying C library
-"""
+try:
+    from ._external import EXTERNAL_METATENSOR_PREFIX
+
+    cmake_prefix_path = EXTERNAL_METATENSOR_PREFIX
+    """
+    Path containing the CMake configuration files for the underlying C library
+    """
+
+except ImportError:
+
+    cmake_prefix_path = os.path.join(os.path.dirname(__file__), "lib", "cmake")
+    """
+    Path containing the CMake configuration files for the underlying C library
+    """
