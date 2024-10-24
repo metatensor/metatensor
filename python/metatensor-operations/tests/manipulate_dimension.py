0df5abb0d963fc9340e115d35d88a165cb5e93f0
ppegolo <paolo.pegolo@epfl.ch>
2024-10-24 13:18:04 +0200

    Add test for renaming components

--- python/metatensor-operations/tests/manipulate_dimension.py
+++ python/metatensor-operations/tests/manipulate_dimension.py
@@ -17,6 +17,11 @@ def tensor():
 
 
 @pytest.fixture
+def tensor_with_components_and_gradients():
+    return metatensor.load(os.path.join(DATA_ROOT, "qm7-spherical-expansion.npz"))
+
+
+@pytest.fixture
 def tensor_extra():
     """TensorMap with gradient containing extra columns in keys, samples, properties"""
     block = TensorBlock(
@@ -236,6 +241,24 @@ def test_rename_properties(tensor):
     for block in new_tensor:
         for _, gradient in block.gradients():
             assert gradient.properties.names[0] == "foo"
+
+
+def test_rename_components(tensor_with_components_and_gradients):
+    new_tensor = metatensor.rename_dimension(
+        tensor_with_components_and_gradients,
+        axis="components",
+        old="o3_mu",
+        new="foo",
+    )
+    assert new_tensor.component_names[0] == "foo"
+
+    for block in new_tensor:
+        for parameter, gradient in block.gradients():
+            if parameter == "positions":
+                gradient_component_names = [["xyz"], ["foo"]]
+            elif parameter == "strain":
+                gradient_component_names = [["xyz_1"], ["xyz_2"], ["foo"]]
+            assert [c.names for c in gradient.components] == gradient_component_names
 
 
 def test_rename_unknown_axis(tensor):
