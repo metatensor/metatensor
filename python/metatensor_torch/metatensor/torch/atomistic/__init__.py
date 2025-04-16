17c604c6543258732f06b8d6bd6e9c68013e7db9
Guillaume Fraux <guillaume.fraux@epfl.ch>
2025-04-16 15:38:46 +0200

    Remove metatensor.torch.atomistic, and re-export metatomic instead

--- python/metatensor_torch/metatensor/torch/atomistic/__init__.py
+++ python/metatensor_torch/metatensor/torch/atomistic/__init__.py
@@ -1,44 +1,55 @@
 import os
+import warnings
 
-import torch
 
+def __getattr__(name):
+    if os.environ.get("METATENSOR_IMPORT_FOR_SPHINX", "0") != "0":
+        from metatomic.torch.documentation import (
+            ModelCapabilities,
+            ModelEvaluationOptions,
+            ModelMetadata,
+            ModelOutput,
+            NeighborListOptions,
+            System,
+            check_atomistic_model,
+            load_model_extensions,
+            read_model_metadata,
+            register_autograd_neighbors,
+            unit_conversion_factor,
+        )
 
-if os.environ.get("METATENSOR_IMPORT_FOR_SPHINX", "0") != "0":
-    from .documentation import (
-        ModelCapabilities,
-        ModelEvaluationOptions,
-        ModelMetadata,
-        ModelOutput,
-        NeighborListOptions,
-        System,
-        check_atomistic_model,
-        load_model_extensions,
-        read_model_metadata,
-        register_autograd_neighbors,
-        unit_conversion_factor,
+    else:
+        from metatomic.torch import (  # noqa: F401
+            ModelCapabilities,
+            ModelEvaluationOptions,
+            ModelMetadata,
+            ModelOutput,
+            NeighborListOptions,
+            System,
+            check_atomistic_model,
+            load_model_extensions,
+            read_model_metadata,
+            register_autograd_neighbors,
+            unit_conversion_factor,
+        )
+
+    from metatomic.torch import (  # noqa: F401
+        MetatensorAtomisticModel,
+        ModelInterface,
+        is_atomistic_model,
+        load_atomistic_model,
+        systems_to_torch,
+    )
+    from metatomic.torch.io import load_system, save  # noqa: F401
+
+    warnings.warn(
+        "importing from metatensor.torch.atomistic is deprecated and will be removed "
+        "in the next release.\nPlease add a depencency on `metatomic[torch]` and "
+        f"import {name!r} from metatomic.torch instead.",
+        stacklevel=2,
     )
 
-else:
-    System = torch.classes.metatensor.System
-    NeighborListOptions = torch.classes.metatensor.NeighborListOptions
+    if name in locals():
+        return locals()[name]
 
-    ModelOutput = torch.classes.metatensor.ModelOutput
-    ModelEvaluationOptions = torch.classes.metatensor.ModelEvaluationOptions
-    ModelCapabilities = torch.classes.metatensor.ModelCapabilities
-    ModelMetadata = torch.classes.metatensor.ModelMetadata
-
-    read_model_metadata = torch.ops.metatensor.read_model_metadata
-    load_model_extensions = torch.ops.metatensor.load_model_extensions
-    check_atomistic_model = torch.ops.metatensor.check_atomistic_model
-
-    register_autograd_neighbors = torch.ops.metatensor.register_autograd_neighbors
-    unit_conversion_factor = torch.ops.metatensor.unit_conversion_factor
-
-from .io import load_system, save  # noqa: F401
-from .model import (  # noqa: F401
-    MetatensorAtomisticModel,
-    ModelInterface,
-    is_atomistic_model,
-    load_atomistic_model,  # noqa: F401
-)
-from .systems_to_torch import systems_to_torch  # noqa: F401
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
