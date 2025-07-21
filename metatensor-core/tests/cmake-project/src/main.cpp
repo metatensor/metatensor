8fbce6104049c02a987316755bfa92829d0002ec
Rohit Goswami <r95g10@gmail.com>
2025-07-22 03:25:42 +0530

    [MAINT] More example cleanup
    Co-authored-by: Guillaume Fraux <luthaf@luthaf.fr>

--- metatensor-core/tests/cmake-project/src/main.cpp
+++ metatensor-core/tests/cmake-project/src/main.cpp
@@ -4,7 +4,6 @@
 int main() {
     try {
         auto labels = metatensor::Labels({"name"}, {{0}, {1}, {2}});
-        auto labels_unchecked = metatensor::Labels({"name_uniq"}, {{0}, {1}, {2}}, metatensor::unchecked_t{});
         return 0;
     } catch (const std::exception&) {
         return 1;
