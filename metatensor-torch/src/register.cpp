a3c2723baf899002cba9fa3573610dea4dd6b0e8
Rohit Goswami <rog32@hi.is>
2025-07-24 15:01:33 +0200

    [ENH] Expose assume_unique through torch reg

--- metatensor-torch/src/register.cpp
+++ metatensor-torch/src/register.cpp
@@ -123,8 +123,20 @@ TORCH_LIBRARY(metatensor, m) {
 
     m.class_<LabelsHolder>("Labels")
         .def(
-            torch::init<torch::IValue, torch::Tensor>(), DOCSTRING,
-            {torch::arg("names"), torch::arg("values")}
+            torch::init([](torch::IValue names, torch::Tensor values,
+                           bool assume_unique) {
+                if (assume_unique) {
+                    return std::make_unique<LabelsHolder>(std::move(names),
+                                                          std::move(values),
+                                                          metatensor::assume_unique{});
+                } else {
+                    return std::make_unique<LabelsHolder>(std::move(names),
+                                                          std::move(values));
+                }
+            }),
+            DOCSTRING,
+            {torch::arg("names"), torch::arg("values"),
+             torch::arg("assume_unique") = false}
         )
         .def("__str__", &LabelsHolder::str)
         .def("__repr__", &LabelsHolder::repr)
