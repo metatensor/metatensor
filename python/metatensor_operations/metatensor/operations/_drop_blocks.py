f0796a5e2a01301dc22a47b538584598fcbba885
alessandroforina <alessandro.forina98@gmail.com>
2025-09-17 15:40:13 +0200

    Update python/metatensor_operations/metatensor/operations/_drop_blocks.py
    Co-authored-by: Guillaume Fraux <luthaf@luthaf.fr>

--- python/metatensor_operations/metatensor/operations/_drop_blocks.py
+++ python/metatensor_operations/metatensor/operations/_drop_blocks.py
@@ -99,7 +99,8 @@ def drop_blocks(tensor: TensorMap, keys: Labels, copy: bool = False) -> TensorMa
 @torch_jit_script
 def drop_empty_blocks(tensor: TensorMap, copy: bool = False) -> TensorMap:
     """
-    Drop every blocks in a :py:class:`TensorMap` with any 0-length dimension.
+    Drop any block in a :py:class:`TensorMap` with an empty set of samples, 
+    components or properties.
 
     :param tensor: the :py:class:`TensorMap` to drop empty blocks from.
 
