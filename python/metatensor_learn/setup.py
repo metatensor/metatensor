3b4a9b20a45f32d1239ec0df50a24d729a948dff
Guillaume Fraux <guillaume.fraux@epfl.ch>
2025-10-29 15:11:01 +0100

    Release metatensor-learn v0.4.0

--- python/metatensor_learn/setup.py
+++ python/metatensor_learn/setup.py
@@ -14,7 +14,7 @@ METATENSOR_OPERATIONS = os.path.realpath(
     os.path.join(ROOT, "..", "metatensor_operations")
 )
 
-METATENSOR_LEARN_VERSION = "0.3.2"
+METATENSOR_LEARN_VERSION = "0.4.0"
 
 
 class bdist_egg_disabled(bdist_egg):
