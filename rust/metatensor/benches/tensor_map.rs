09e9cb2ac597db6a9aa57bcacb8aed09bc93602e
Guillaume Fraux <guillaume.fraux@epfl.ch>
2026-02-16 14:48:26 +0100

    Add a benchmark for keys_to_samples and keys_to_properties

---
+++ rust/metatensor/benches/tensor_map.rs
@@ -0,0 +1,91 @@
+#[cfg(not(feature = "bench"))]
+compile_error!("the bench feature is required for bencharks, use `cargo bench --features bench`");
+
+#[cfg(feature = "bench")]
+mod benchmarks {
+    use criterion::{BatchSize, BenchmarkId, Criterion};
+    pub use criterion::{criterion_group, criterion_main};
+    use metatensor::{Labels, LabelsBuilder, TensorBlock, TensorMap};
+    use ndarray::ArcArray;
+
+    fn tensor_map(n_blocks: usize, n_samples: usize, n_properties: usize) -> TensorMap {
+        let mut keys = LabelsBuilder::new(vec!["key_1", "key_2"]);
+        for i in 0..n_blocks {
+            keys.add(&[i, 0]);
+        }
+        let keys = keys.finish();
+
+        let mut blocks = Vec::new();
+        for _ in 0..n_blocks {
+            let mut samples = LabelsBuilder::new(vec!["samples"]);
+            for i in 0..n_samples {
+                samples.add(&[i]);
+            }
+            let samples = samples.finish();
+
+            let mut components_builder = LabelsBuilder::new(vec!["components"]);
+            components_builder.add(&[0]);
+            let components = [components_builder.finish()];
+
+            let mut properties = LabelsBuilder::new(vec!["properties"]);
+            for i in 0..n_properties {
+                properties.add(&[i]);
+            }
+            let properties = properties.finish();
+
+            let shape = vec![samples.count(), components[0].count(), properties.count()];
+            let data = ArcArray::from_elem(shape, 1.0);
+
+            blocks.push(TensorBlock::new(data, &samples, &components, &properties).unwrap());
+        }
+
+        TensorMap::new(keys, blocks).unwrap()
+    }
+
+    pub fn keys_to_samples(c: &mut Criterion) {
+        let mut group = c.benchmark_group("TensorMap::keys_to_samples");
+        // reduce sample size for faster benchmark execution during dev/test
+        group.sample_size(10);
+
+        for n_blocks in [1, 10, 100] {
+            let tensor = tensor_map(n_blocks, 1000, 1000);
+            let keys_to_move = Labels::empty(vec!["key_1"]);
+            group.bench_function(BenchmarkId::new("n_blocks", n_blocks), |b| {
+                b.iter_batched(
+                    || tensor.try_clone().unwrap(),
+                    |tensor| std::hint::black_box(tensor.keys_to_samples(&keys_to_move, true)),
+                    BatchSize::LargeInput,
+                );
+            });
+        }
+        group.finish();
+    }
+
+    pub fn keys_to_properties(c: &mut Criterion) {
+        let mut group = c.benchmark_group("TensorMap::keys_to_properties");
+        group.sample_size(10);
+
+        for n_blocks in [1, 10, 100] {
+            let tensor = tensor_map(n_blocks, 1000, 1000);
+            let keys_to_move = Labels::empty(vec!["key_1"]);
+            group.bench_function(BenchmarkId::new("n_blocks", n_blocks), |b| {
+                b.iter_batched(
+                    || tensor.try_clone().unwrap(),
+                    |tensor| std::hint::black_box(tensor.keys_to_properties(&keys_to_move, true)),
+                    BatchSize::LargeInput,
+                );
+            });
+        }
+        group.finish();
+    }
+}
+
+
+#[cfg(feature = "bench")]
+benchmarks::criterion_group!(benches, benchmarks::keys_to_samples, benchmarks::keys_to_properties);
+
+#[cfg(feature = "bench")]
+benchmarks::criterion_main!(benches);
+
+#[cfg(not(feature = "bench"))]
+fn main() {}
