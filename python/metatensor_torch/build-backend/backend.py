1a501d0d30aba2f000ddfb8cc18f7289170d90e0
Guillaume Fraux <guillaume.fraux@epfl.ch>
2026-03-25 14:13:07 +0100

    Run tests on Python 3.14 (#1040)
    * Run tests on Python 3.14
    
    Co-Authored-By: GardevoirX <xqj.2001@outlook.com>
    
    * Use pathlib in setup.py instead of os.path
    
    The previous code was creating malformed path on windows when
    using string concatenation with `file://`, which Path.as_uri does
    correctly.
    
    ---------
    
    Co-authored-by: GardevoirX <xqj.2001@outlook.com>

--- python/metatensor_torch/build-backend/backend.py
+++ python/metatensor_torch/build-backend/backend.py
@@ -5,12 +5,13 @@
 # This also allows to only depend on torch/cmake when building the wheel and not the
 # sdist
 import os
+import pathlib
 
 from setuptools import build_meta
 
 
-ROOT = os.path.realpath(os.path.dirname(__file__))
-METATENSOR_CORE = os.path.realpath(os.path.join(ROOT, "..", "..", "metatensor_core"))
+ROOT = pathlib.Path(__file__).parent.resolve()
+METATENSOR_CORE = (ROOT / ".." / ".." / "metatensor_core").resolve()
 FORCED_METATENSOR_CORE_VERSION = os.environ.get(
     "METATENSOR_TORCH_BUILD_WITH_METATENSOR_CORE_VERSION"
 )
@@ -22,9 +23,9 @@ if FORCED_METATENSOR_CORE_VERSION is not None:
     # from a sdist on a non-released version
     METATENSOR_CORE_DEP = f"metatensor-core =={FORCED_METATENSOR_CORE_VERSION}"
 
-elif not METATENSOR_NO_LOCAL_DEPS and os.path.exists(METATENSOR_CORE):
+elif not METATENSOR_NO_LOCAL_DEPS and METATENSOR_CORE.exists():
     # we are building from a git checkout
-    METATENSOR_CORE_DEP = f"metatensor-core @ file://{METATENSOR_CORE}"
+    METATENSOR_CORE_DEP = f"metatensor-core @ {METATENSOR_CORE.as_uri()}"
 else:
     # we are building from a sdist
     METATENSOR_CORE_DEP = "metatensor-core >=0.1.18,<0.2.0"
